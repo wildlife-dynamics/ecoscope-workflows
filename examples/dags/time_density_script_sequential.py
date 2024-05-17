3a84fdcef8fc27882856d1ddc27f0b9b3d39d883
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-05-16 22:13:02 -0700

    sequential script WIP cont

--- examples/dags/time_density_script_sequential.py
+++ examples/dags/time_density_script_sequential.py
@@ -1,12 +1,24 @@
+import argparse
+import yaml
 from ecoscope_workflows.tasks.python.io import get_subjectgroup_observations
 from ecoscope_workflows.tasks.python.preprocessing import process_relocations
+
 if __name__ == "__main__":
-    # argparse arguments from jsonschema
+    parser = argparse.ArgumentParser()
+    g = parser.add_argument_group('calculate_time_density')
+    g.add_argument(
+        '--config-file',
+        dest='config_file',
+        required=True,
+        type=argparse.FileType(mode='r'),
+    )
+    args = parser.parse_args()
+    # TODO: omit fields which are listed in arg_dependencies at the TaskInstance level
+    params = yaml.safe_load(args.config_file)
     # FIXME: first pass assumes tasks are already in topological order
     
-    get_subjectgroup_observations_return = get_subjectgroup_observations.replace(validate=True)()
+    get_subjectgroup_observations_return = get_subjectgroup_observations.replace(validate=True)(params["get_subjectgroup_observations"])
     
     process_relocations_return = process_relocations.replace(validate=True)(
         observations=get_subjectgroup_observations_return,
-    )
-    
+        **params["process_relocations"])
