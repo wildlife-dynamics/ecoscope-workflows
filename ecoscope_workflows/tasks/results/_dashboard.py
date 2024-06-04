0cda62e17a714b218350bb964ab342f7a685524f
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-06-04 12:48:43 -0700

    maybe keyed result is a generic

--- ecoscope_workflows/tasks/results/_dashboard.py
+++ ecoscope_workflows/tasks/results/_dashboard.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import Generic, TypeVar
+
+from ecoscope_workflows.decorators import distributed
 
 
 @dataclass
@@ -10,9 +13,9 @@ class Groupers:
 
 @dataclass(frozen=True)
 class GroupKey:
-    a: str | None
-    b: str | None
-    c: str | None
+    a: str | None = None
+    b: str | None = None
+    c: str | None = None
 
 
 @dataclass
@@ -26,10 +29,70 @@ class Map:
 
 
 @dataclass
+class Plot:
+    path: str
+
+
+@dataclass
+class Table: ...
+
+
+@dataclass
+class StaticImage:
+    path: str
+
+
+@dataclass
+class Calendar: ...
+
+
+@dataclass
+class Text:
+    text: str
+
+
+Result = TypeVar("Result", SingleValue, Map, Plot, Table, StaticImage, Calendar, Text)
+
+
+@dataclass
+class KeyedResult(Generic[Result]):
+    key: GroupKey
+    result: Result
+
+
+@dataclass
 class PerGroupPatrolReportDashboard:
+    groupers: Groupers
+    # trajectories results
     mean_time: dict[GroupKey, SingleValue]
     total_time: dict[GroupKey, SingleValue]
     total_distance: dict[GroupKey, SingleValue]
     mean_distance: dict[GroupKey, SingleValue]
     trajectory_map: dict[GroupKey, Map]
-    time_density: ...  # does not have a group key
+    time_density_map: Map
+    # events results
+
+
+@distributed
+def make_patrols_dashboard(
+    trajectory_grouped_single_values: dict[str, dict[GroupKey, SingleValue]],
+    trajectory_grouped_maps: dict[GroupKey, Map],
+    time_density_map: Map,
+    events_grouped_plots: dict[GroupKey, Plot],
+    events_non_grouped_plot: Plot,
+) -> PerGroupPatrolReportDashboard:
+    groupers: Groupers = ...  # infer groupers from grouped data indexes here
+    return PerGroupPatrolReportDashboard(
+        groupers=groupers,
+        # trajectories results
+        mean_time=trajectory_grouped_single_values["mean_time"],
+        total_time=trajectory_grouped_single_values["total_time"],
+        total_distance=trajectory_grouped_single_values["total_distance"],
+        mean_distance=trajectory_grouped_single_values["mean_distance"],
+        trajectory_grouped_maps=trajectory_grouped_maps,
+        # NOTE: use null GroupKey to store non-grouped results
+        time_density_map={GroupKey(): time_density_map},
+        # events results
+        events_grouped_plots=events_grouped_plots,
+        events_non_grouped_plot={GroupKey(): events_non_grouped_plot},
+    )
