8bd197e130baa9d29a621d176939274d7bf63aa7
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-06-18 16:23:12 -0700

    workaround for FieldInfo __eq__

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,19 +1,62 @@
+from pydantic.fields import FieldInfo
+
 from ecoscope_workflows.registry import KnownTask, KubernetesPodOperator
 
 
-def test_client_model_fields():
+def test_connections_model_fields():
     importable_reference = "ecoscope_workflows.tasks.io.get_subjectgroup_observations"
     kt = KnownTask(
         importable_reference=importable_reference,
         operator=KubernetesPodOperator(image="", name="", container_resources={}),
     )
-    assert kt.client_model_fields == {
-        "client": {
-            "description": "A named EarthRanger connection.",
-            "title": "Client",
-            "type": "string",
-        }
-    }
+
+    def _assert_equal(a, b):
+        # pydantic.fields.FieldInfo does not implement __eq__, so this is a workaround
+        assert set(a) == set(b)
+        assert a[next(iter(a))]["type"] == b[next(iter(b))]["type"]
+        a_fields = a[next(iter(a))]["fields"]
+        b_fields = b[next(iter(b))]["fields"]
+        assert isinstance(a_fields, dict)
+        assert isinstance(b_fields, dict)
+        assert set(a_fields) == set(b_fields)
+        for k, v in a_fields.items():
+            assert str(v) == str(b_fields[k])
+
+    _assert_equal(
+        kt.connections_model_fields,
+        {
+            "client": {
+                "type": "EarthRangerConnection",
+                "fields": {
+                    "password": FieldInfo(
+                        annotation=str,
+                        required=True,
+                        description="EarthRanger password",
+                    ),
+                    "server": FieldInfo(
+                        annotation=str,
+                        required=True,
+                        description="URL for EarthRanger API",
+                    ),
+                    "sub_page_size": FieldInfo(
+                        annotation=int,
+                        required=True,
+                        description="Sub page size for API requests",
+                    ),
+                    "tcp_limit": FieldInfo(
+                        annotation=int,
+                        required=True,
+                        description="TCP limit for API requests",
+                    ),
+                    "username": FieldInfo(
+                        annotation=str,
+                        required=True,
+                        description="EarthRanger username",
+                    ),
+                },
+            }
+        },
+    )
 
 
 def test_known_task_parameters_jsonschema():
