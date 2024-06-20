4de40042983c25d25850a4e4e84fc00b559b227d
Yun Wu <ywu910318@gmail.com>
2024-06-20 14:52:17 +0800

    Add the task to registry

--- ecoscope_workflows/tasks/__init__.py
+++ ecoscope_workflows/tasks/__init__.py
@@ -2,3 +2,4 @@ from . import analysis as analysis
 from . import io as io
 from . import preprocessing as preprocessing
 from . import results as results
+from . import transformation as transformation
