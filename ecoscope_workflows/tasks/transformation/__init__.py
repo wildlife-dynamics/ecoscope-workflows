7f4ac5025c540fde0d5a342313eb4327a3e9e1da
Yun Wu <ywu910318@gmail.com>
2024-06-21 11:47:26 +0800

    add a classification task

--- ecoscope_workflows/tasks/transformation/__init__.py
+++ ecoscope_workflows/tasks/transformation/__init__.py
@@ -1,5 +1,7 @@
+from ._classification import classify_categorical_value
 from ._column_mapping import map_columns
 
 __all__ = [
     "map_columns",
+    "classify_categorical_value",
 ]
---
+++ ecoscope_workflows/tasks/transformation/_classification.py
@@ -0,0 +1,48 @@
+from typing import Annotated
+
+from pydantic import Field
+
+from ecoscope_workflows.annotations import DataFrame, JsonSerializableDataFrameModel
+from ecoscope_workflows.decorators import distributed
+
+
+@distributed
+def classify_categorical_value(
+    df: DataFrame[JsonSerializableDataFrameModel],
+    column_name: Annotated[
+        str, Field(description="The name of the column with categorical values.")
+    ],
+    colormap_name: Annotated[
+        str, Field(default="viridis", description="matplotlib.colors.Colormap")
+    ],
+) -> DataFrame[JsonSerializableDataFrameModel]:
+    """
+    Adds a color column to the dataframe based on the categorical values in the specified column.
+
+    Args:
+    dataframe (pd.DataFrame): The input dataframe.
+    column_name (str): The name of the column with categorical values.
+    colormap_name (str): The name of the matplotlib colormap to use.
+
+    Returns:
+    pd.DataFrame: The dataframe with an additional color column.
+    """
+
+    import matplotlib.cm as cm
+
+    # Get the colormap
+    colormap = cm.get_cmap(colormap_name)
+
+    # Get unique categories
+    unique_categories = df[column_name].unique()
+
+    # Create a dictionary to map categories to colors
+    colors = colormap(range(len(unique_categories)))
+    category_colors = {
+        category: colors[i] for i, category in enumerate(unique_categories)
+    }
+
+    # Add the color column to the dataframe
+    df["label"] = df[column_name].map(category_colors)
+
+    return df
