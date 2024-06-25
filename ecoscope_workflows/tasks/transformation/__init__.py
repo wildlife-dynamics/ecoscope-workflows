1bb68288d033f32c1bcf6b7d8ff1ed0f1c92066f
Yun Wu <ywu910318@gmail.com>
2024-06-25 10:26:35 +0800

    map values

--- ecoscope_workflows/tasks/transformation/__init__.py
+++ ecoscope_workflows/tasks/transformation/__init__.py
@@ -1,7 +1,5 @@
 from ._classification import color_map
 from ._column_mapping import map_columns
+from ._value_mapping import map_values
 
-__all__ = [
-    "map_columns",
-    "color_map",
-]
+__all__ = ["map_columns", "color_map", "map_values"]
