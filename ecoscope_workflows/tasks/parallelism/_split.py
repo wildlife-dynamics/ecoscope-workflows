e21b4c41e95976e4927c51450000b674c3822aaf
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-02 16:20:59 -0700

    move split groups logic into task

--- ecoscope_workflows/tasks/parallelism/_split.py
+++ ecoscope_workflows/tasks/parallelism/_split.py
@@ -5,12 +5,25 @@ import pandera as pa
 
 from ecoscope_workflows.annotations import JsonSerializableDataFrameModel
 from ecoscope_workflows.decorators import distributed
+from ecoscope_workflows.serde import (
+    groupbykeys_to_hivekeys,
+    persist_gdf_to_hive_partitioned_parquet,
+)
 
 
 @distributed
 def split_groups(
     dataframe: Annotated[pa.typing.DataFrame[JsonSerializableDataFrameModel], Field()],
     groupers: Annotated[list[str], Field()],
-) -> Annotated[dict, Field()]:
-    # FIXME: this can actually be .groupby(levels=[1, 2, 3]) if we have a heirarchical index
-    return {k: v for k, v in dataframe.groupby(groupers)}
+    cache_path: Annotated[str, Field()],
+) -> Annotated[list[dict[str, tuple[tuple[tuple[str, str, str], ...], str]]], Field()]:
+    df_url = dataframe.pipe(
+        persist_gdf_to_hive_partitioned_parquet,
+        partition_on=groupers,
+        path=cache_path,
+    )
+    return [
+        {"element": (nested_hk, str(df_url))}
+        for nested_hk in groupbykeys_to_hivekeys(dataframe, groupers)
+        # {"element": ((('animal_name', '=', 'Bo'), ('month', '=', 'January')), "gcs://bucket/tmp/job-3456788/tmp.parquet")}
+    ]
