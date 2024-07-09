8ebac2914c180477949cb6afc67833e64cf4ab93
Alex Morling <atmorling@gmail.com>
2024-07-09 11:52:09 +1000

    Update Ecomap task

--- ecoscope_workflows/tasks/results/_ecomap.py
+++ ecoscope_workflows/tasks/results/_ecomap.py
@@ -1,4 +1,5 @@
-from typing import Annotated
+from dataclasses import dataclass
+from typing import Annotated, Literal
 
 from pydantic import Field
 
@@ -6,29 +7,46 @@ from ecoscope_workflows.annotations import AnyGeoDataFrame
 from ecoscope_workflows.decorators import distributed
 
 
+@dataclass
+class LayerDefinition:
+    data: AnyGeoDataFrame
+    layer_type: Literal["Scatterplot", "Path", "Polygon"]
+    style_kws: dict
+
+
 @distributed
 def draw_ecomap(
-    geodataframe: AnyGeoDataFrame,
-    static: Annotated[bool, Field()],
-    height: Annotated[int, Field()],
-    width: Annotated[int, Field()],
-    search_control: Annotated[bool, Field()],
-    title: Annotated[str, Field()],
-    title_kws: Annotated[dict, Field()],
-    tile_layers: Annotated[list[dict], Field()],
-    north_arrow_kws: Annotated[dict, Field()],
-    add_gdf_kws: Annotated[dict, Field()],
+    layers: Annotated[list[LayerDefinition] | LayerDefinition, Field()],
+    tile_layer: Annotated[str | None, Field()] = None,
+    static: Annotated[bool, Field()] = False,
+    title: Annotated[str | None, Field()] = None,
+    title_kws: Annotated[dict, Field()] = {},
+    scale_kws: Annotated[dict, Field()] = {},
+    north_arrow_kws: Annotated[dict, Field()] = {},
 ) -> Annotated[str, Field()]:
     from ecoscope.mapping import EcoMap
 
-    m = EcoMap(static=static, height=height, width=width, search_control=search_control)
-    m.add_title(title=title, **title_kws)
+    m = EcoMap(static=static, default_widgets=False)
 
-    for tl in tile_layers:
-        m.add_tile_layer(**tl)
+    if title:
+        m.add_title(title, **title_kws)
 
+    m.add_scale_bar(**scale_kws)
     m.add_north_arrow(**north_arrow_kws)
-    m.add_gdf(geodataframe, **add_gdf_kws)
-    m.zoom_to_gdf(geodataframe)
 
-    return m._repr_html_(fill_parent=True)
+    if tile_layer is not None:
+        m.add_layer(EcoMap.get_named_tile_layer(tile_layer))
+
+    if isinstance(layers, LayerDefinition):
+        layers = [layers]
+    for layer in layers:
+        match layer.layer_type:
+            case "Scatterplot":
+                m.add_scatterplot_layer(layer.data, **layer.style_kws)
+            case "Path":
+                m.add_path_layer(layer.data, **layer.style_kws)
+            case "Polygon":
+                m.add_polygon_layer(layer.data, **layer.style_kws)
+
+    m.zoom_to_bounds(m.layers)
+    return m.to_html()
