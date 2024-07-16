89154b160d95288c8e4d3c4232fc2944b2fa0f89
Yun Wu <ywu910318@gmail.com>
2024-07-16 21:59:49 +0800

    first try to create patrol workflow

---
+++ examples/dags/patrol_workflow.py
@@ -0,0 +1,47 @@
+import argparse
+import yaml
+
+from ecoscope_workflows.tasks.io import get_patrol_observations
+from ecoscope_workflows.tasks.preprocessing import process_relocations
+from ecoscope_workflows.tasks.preprocessing import relocations_to_trajectory
+from ecoscope_workflows.tasks.analysis import calculate_time_density
+from ecoscope_workflows.tasks.results import draw_ecomap
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    g = parser.add_argument_group("patrol_workflow")
+    g.add_argument(
+        "--config-file",
+        dest="config_file",
+        required=True,
+        type=argparse.FileType(mode="r"),
+    )
+    args = parser.parse_args()
+    params = yaml.safe_load(args.config_file)
+    # FIXME: first pass assumes tasks are already in topological order
+
+    get_patrol_observations_return = get_patrol_observations.replace(validate=True)(
+        **params["get_patrol_observations"],
+    )
+
+    process_relocations_return = process_relocations.replace(validate=True)(
+        observations=get_patrol_observations_return,
+        **params["process_relocations"],
+    )
+
+    relocations_to_trajectory_return = relocations_to_trajectory.replace(validate=True)(
+        relocations=process_relocations_return,
+        **params["relocations_to_trajectory"],
+    )
+
+    calculate_time_density_return = calculate_time_density.replace(validate=True)(
+        trajectory_gdf=relocations_to_trajectory_return,
+        **params["calculate_time_density"],
+    )
+
+    draw_ecomap_return = draw_ecomap.replace(validate=True)(
+        geodataframe=calculate_time_density_return,
+        **params["draw_ecomap"],
+    )
+
+    print(draw_ecomap_return)
