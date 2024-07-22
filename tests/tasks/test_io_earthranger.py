36054da151bb930a0a10c31001748bb3cc332144
Yun Wu <ywu910318@gmail.com>
2024-07-22 21:10:20 +0800

    fix unittest

--- tests/tasks/test_io_earthranger.py
+++ tests/tasks/test_io_earthranger.py
@@ -15,15 +15,13 @@ pytestmark = pytest.mark.io
 
 @pytest.fixture
 def client():
-    return (
-        EarthRangerConnection(
-            server=os.environ["EARTHRANGER_SERVER"],
-            username=os.environ["EARTHRANGER_USERNAME"],
-            password=os.environ["EARTHRANGER_PASSWORD"],
-            tcp_limit="5",
-            sub_page_size="4000",
-        ).get_client(),
-    )
+    return EarthRangerConnection(
+        server=os.environ["EARTHRANGER_SERVER"],
+        username=os.environ["EARTHRANGER_USERNAME"],
+        password=os.environ["EARTHRANGER_PASSWORD"],
+        tcp_limit="5",
+        sub_page_size="4000",
+    ).get_client()
 
 
 def test_get_subject_group_observations(client):
@@ -36,6 +34,10 @@ def test_get_subject_group_observations(client):
     )
 
     assert len(result) > 0
+    assert "geometry" in result
+    assert "groupby_col" in result
+    assert "fixtime" in result
+    assert "junk_status" in result
 
 
 def test_get_patrol_observations(client):
@@ -44,12 +46,16 @@ def test_get_patrol_observations(client):
         since="2011-01-01",
         until="2023-01-01",
         # MEP_Distance_Survey_Patrol
-        patrol_type=["0ef3bf48-b44c-4a4e-a145-7ab2e38c9a57"],
+        patrol_type="0ef3bf48-b44c-4a4e-a145-7ab2e38c9a57",
         status=None,
         include_patrol_details=True,
     )
 
     assert len(result) > 0
+    assert "geometry" in result
+    assert "groupby_col" in result
+    assert "fixtime" in result
+    assert "junk_status" in result
 
 
 def test_get_patrol_events(client):
@@ -57,9 +63,11 @@ def test_get_patrol_events(client):
         client=client,
         since="2011-01-01",
         until="2025-01-01",
-        # eric_patrol
-        patrol_type=["804c75f0-ed55-4bf5-b6fc-1aaf166bcf84"],
+        patrol_type=None,
         status=None,
     )
 
     assert len(result) > 0
+    assert "id" in result
+    assert "event_type" in result
+    assert "geometry" in result
