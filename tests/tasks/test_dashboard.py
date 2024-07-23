378e2ece40ea5be746646efa0676a070aac8eacd
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-23 15:10:10 -0700

    test views json

--- tests/tasks/test_dashboard.py
+++ tests/tasks/test_dashboard.py
@@ -64,59 +64,38 @@ def test__get_view(single_filter_dashboard: Dashboard):
     ]
 
 
-def test_views_json():
-    great_map = GroupedWidget(
-        widget_type="map",
-        title="A Great Map",
-        views={
-            (("month", "=", "january"),): "/path/to/precomputed/jan/map.html",
-            (("month", "=", "february"),): "/path/to/precomputed/feb/map.html",
-        },
-    )
-    cool_plot = GroupedWidget(
-        widget_type="plot",
-        title="A Cool Plot",
-        views={
-            (("month", "=", "january"),): "/path/to/precomputed/jan/plot.html",
-            (("month", "=", "february"),): "/path/to/precomputed/feb/plot.html",
-        },
-    )
-    dashboard = Dashboard(
-        groupers={"month": ["january", "february"]},
-        keys=[
-            (("month", "=", "january"),),
-            (("month", "=", "february"),),
+def test_views_json(single_filter_dashboard: Dashboard):
+    dashboard = single_filter_dashboard
+    assert dashboard.views_json == {
+        '{"month": "january"}': [
+            EmumeratedWidgetView(
+                id=0,
+                widget_type="map",
+                title="A Great Map",
+                data="/path/to/precomputed/jan/map.html",
+            ),
+            EmumeratedWidgetView(
+                id=1,
+                widget_type="plot",
+                title="A Cool Plot",
+                data="/path/to/precomputed/jan/plot.html",
+            ),
         ],
-        widgets=[great_map, cool_plot],
-    )
-    assert dashboard._get_view((("month", "=", "january"),)) == [
-        EmumeratedWidgetView(
-            id=0,
-            widget_type="map",
-            title="A Great Map",
-            data="/path/to/precomputed/jan/map.html",
-        ),
-        EmumeratedWidgetView(
-            id=1,
-            widget_type="plot",
-            title="A Cool Plot",
-            data="/path/to/precomputed/jan/plot.html",
-        ),
-    ]
-    assert dashboard._get_view((("month", "=", "february"),)) == [
-        EmumeratedWidgetView(
-            id=0,
-            widget_type="map",
-            title="A Great Map",
-            data="/path/to/precomputed/feb/map.html",
-        ),
-        EmumeratedWidgetView(
-            id=1,
-            widget_type="plot",
-            title="A Cool Plot",
-            data="/path/to/precomputed/feb/plot.html",
-        ),
-    ]
+        '{"month": "february"}': [
+            EmumeratedWidgetView(
+                id=0,
+                widget_type="map",
+                title="A Great Map",
+                data="/path/to/precomputed/feb/map.html",
+            ),
+            EmumeratedWidgetView(
+                id=1,
+                widget_type="plot",
+                title="A Cool Plot",
+                data="/path/to/precomputed/feb/plot.html",
+            ),
+        ],
+    }
 
 
 def test__get_view_two_part_key():
