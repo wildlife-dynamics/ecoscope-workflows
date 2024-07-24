0ef92ac99b9e36d1a36f44a4a3a4e19f8d09e7d8
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-24 15:25:11 -0700

    fixture

--- tests/test_compiler.py
+++ tests/test_compiler.py
@@ -1,5 +1,6 @@
 from textwrap import dedent
 
+import pytest
 import yaml
 
 from ecoscope_workflows.compiler import DagCompiler, Spec, TaskInstance
@@ -13,18 +14,22 @@ def test_task_instance_known_task_parsing():
     assert ti.known_task == known_tasks[task_name]
 
 
-def test_dag_compiler_from_spec():
+@pytest.fixture
+def spec_dict() -> dict:
     spec_str = dedent(
-        """\s
-    name: calculate_time_density
-    cache_root: gcs://my-bucket/ecoscope/cache/dag-runs
-    tasks:
-        get_subjectgroup_observations: {}
-        process_relocations:
-            observations: get_subjectgroup_observations
-    """
+        """\
+        name: calculate_time_density
+        cache_root: gcs://my-bucket/ecoscope/cache/dag-runs
+        tasks:
+            get_subjectgroup_observations: {}
+            process_relocations:
+                observations: get_subjectgroup_observations
+        """
     )
-    spec_dict = yaml.safe_load(spec_str)
+    return yaml.safe_load(spec_str)
+
+
+def test_dag_compiler_from_spec(spec_dict: dict):
     spec = Spec.from_spec_file(spec=spec_dict)
     dc = DagCompiler(spec=spec)
-    assert isinstance(dc.spec.tasks["get_subjectgroup_observations"], TaskInstance)
+    assert isinstance(dc.spec.tasks[0], TaskInstance)
