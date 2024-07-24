d5bd531543facb8463fd7815b0556dcdd8922681
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-24 16:12:24 -0700

    simplify

--- ecoscope_workflows/compiler.py
+++ ecoscope_workflows/compiler.py
@@ -1,20 +1,16 @@
 import functools
 import pathlib
 import subprocess
-from typing import Any, Callable
+from typing import Callable
 
-import pandera as pa
 from jinja2 import Environment, FileSystemLoader
 from pydantic import (
     BaseModel,
     Field,
-    FieldSerializationInfo,
     computed_field,
-    field_serializer,
 )
 
-from ecoscope_workflows.registry import KnownTask, known_deserializers, known_tasks
-from ecoscope_workflows.annotations import is_subscripted_pandera_dataframe
+from ecoscope_workflows.registry import KnownTask, known_tasks
 
 
 TEMPLATES = pathlib.Path(__file__).parent / "templates"
@@ -23,8 +19,6 @@ TEMPLATES = pathlib.Path(__file__).parent / "templates"
 class TaskInstance(BaseModel):
     known_task_name: str  # TODO: validate is valid key in known_tasks
     arg_dependencies: dict = Field(default_factory=dict)
-    # TODO: something about defaulting to registered deserializers
-    # by arg type (via introspection); just manual for now
     arg_prevalidators: dict = Field(default_factory=dict)
     return_postvalidator: Callable | None = None
 
@@ -34,15 +28,6 @@ class TaskInstance(BaseModel):
         kt = known_tasks[self.known_task_name]
         assert self.known_task_name == kt.function
         return kt
-
-    @field_serializer("arg_prevalidators")
-    def serialize_arg_prevalidators(self, v: Any, info: FieldSerializationInfo):
-        context: dict = info.context
-        if context:
-            pass
-        return {
-            arg_name: func.__name__ for arg_name, func in self.arg_prevalidators.items()
-        }
 
 
 def ruff_formatted(returns_str_func: Callable[..., str]) -> Callable:
@@ -78,31 +63,16 @@ class Spec(BaseModel):
         tasks = []
         for task_name in self.workflow:
             arg_dependencies: dict[str, str] = {}
-            arg_prevalidators: dict[str, Callable] = {}
             # if the value of the task is None, the task has no dependencies
             if self.workflow[task_name]:
                 # if the value is a dict, then then that dict's k:v pairs are the
                 # arg on the task mapped to the dependency to deserialize it from
                 for arg, dep in self.workflow[task_name].items():
                     arg_dependencies |= {arg: f"{dep}_return"}
-                    # now we have to figure out how to deserialize this arg when passed
-                    # FIXME: this factoring seems sub-optimal; ideally we only introspect the
-                    # signature (which triggers import, and might be slow) once per compilation.
-                    arg_type = known_tasks[task_name].params_annotations_args[arg][0]
-                    if is_subscripted_pandera_dataframe(arg_type):
-                        # NOTE: even if an argument is passed as as an `arg_dependency`, we don't need a
-                        # pre-validator unless the value passed needs some custom logic for deserialization
-                        # (e.g. being loaded from a storage device, etc.). For just strings that Pydantic
-                        # will know how to parse in built-in/obvious ways, this is not needed.
-                        arg_prevalidators |= {
-                            arg: known_deserializers[pa.typing.DataFrame]
-                        }
-                        # TODO: right now the only custom type we're handling is the dataframe, let's add others soon!
             tasks.append(
                 TaskInstance(
                     known_task_name=task_name,
                     arg_dependencies=arg_dependencies,
-                    arg_prevalidators=arg_prevalidators,
                 )
             )
         return tasks
