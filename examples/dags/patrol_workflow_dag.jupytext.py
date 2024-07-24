37e8233e839d597d8d5188a6591cd88dc60cbf49
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-24 06:33:12 -0700

    Drop AST parsing for Jupytext target in favor of calling task functions directly (#98)

--- examples/dags/patrol_workflow_dag.jupytext.py
+++ examples/dags/patrol_workflow_dag.jupytext.py
@@ -5,185 +5,115 @@
 # TODO: top level description
 
 # %% [markdown]
+# ## Imports
+
+from ecoscope_workflows.tasks.io import get_patrol_observations
+from ecoscope_workflows.tasks.preprocessing import process_relocations
+from ecoscope_workflows.tasks.preprocessing import relocations_to_trajectory
+from ecoscope_workflows.tasks.results import draw_ecomap
+from ecoscope_workflows.tasks.io import persist_text
+
+# %% [markdown]
 # ## Get Patrol Observations
 
 # %%
 # parameters
 
-client = ...
-since = ...
-until = ...
-patrol_type = ...
-status = ...
-include_patrol_details = ...
-
-
-# %%
-# the code for Get Patrol Observations
-
-"Get observations for a patrol type from EarthRanger."
-
-# %%
-# return value from this section
-
-get_patrol_observations_return = client.get_patrol_observations_with_patrol_filter(
-    since=since,
-    until=until,
-    patrol_type=patrol_type,
-    status=status,
-    include_patrol_details=include_patrol_details,
+get_patrol_observations_params = dict(
+    client=...,
+    since=...,
+    until=...,
+    patrol_type=...,
+    status=...,
+    include_patrol_details=...,
 )
 
+# %%
+# call the task
+
+get_patrol_observations_return = get_patrol_observations(
+    **get_patrol_observations_params,
+)
 
 # %% [markdown]
 # ## Process Relocations
-# %%
-# dependencies assignments
-
-observations = get_patrol_observations_return
-
 
 # %%
 # parameters
 
-filter_point_coords = ...
-relocs_columns = ...
-
-
-# %%
-# the code for Process Relocations
-
-from ecoscope.base import RelocsCoordinateFilter, Relocations
-
-relocs = Relocations(observations)
-relocs.apply_reloc_filter(
-    RelocsCoordinateFilter(filter_point_coords=filter_point_coords), inplace=True
+process_relocations_params = dict(
+    filter_point_coords=...,
+    relocs_columns=...,
 )
-relocs.remove_filtered(inplace=True)
-relocs = relocs[relocs_columns]
-relocs.columns = [i.replace("extra__", "") for i in relocs.columns]
-relocs.columns = [i.replace("subject__", "") for i in relocs.columns]
 
 # %%
-# return value from this section
+# call the task
 
-process_relocations_return = relocs
-
-
+process_relocations_return = process_relocations(
+    observations=get_patrol_observations_return,
+    **process_relocations_params,
+)
 # %% [markdown]
 # ## Relocations To Trajectory
-# %%
-# dependencies assignments
-
-relocations = process_relocations_return
-
 
 # %%
 # parameters
 
-min_length_meters = ...
-max_length_meters = ...
-max_time_secs = ...
-min_time_secs = ...
-max_speed_kmhr = ...
-min_speed_kmhr = ...
-
-
-# %%
-# the code for Relocations To Trajectory
-
-from ecoscope.base import Relocations
-from ecoscope.base import Trajectory, TrajSegFilter
-
-traj = Trajectory.from_relocations(Relocations(relocations))
-traj_seg_filter = TrajSegFilter(
-    min_length_meters=min_length_meters,
-    max_length_meters=max_length_meters,
-    min_time_secs=min_time_secs,
-    max_time_secs=max_time_secs,
-    min_speed_kmhr=min_speed_kmhr,
-    max_speed_kmhr=max_speed_kmhr,
+relocations_to_trajectory_params = dict(
+    min_length_meters=...,
+    max_length_meters=...,
+    max_time_secs=...,
+    min_time_secs=...,
+    max_speed_kmhr=...,
+    min_speed_kmhr=...,
 )
-traj.apply_traj_filter(traj_seg_filter, inplace=True)
-traj.remove_filtered(inplace=True)
 
 # %%
-# return value from this section
+# call the task
 
-relocations_to_trajectory_return = traj
-
-
+relocations_to_trajectory_return = relocations_to_trajectory(
+    relocations=process_relocations_return,
+    **relocations_to_trajectory_params,
+)
 # %% [markdown]
 # ## Draw Ecomap
-# %%
-# dependencies assignments
-
-geodataframe = relocations_to_trajectory_return
-
 
 # %%
 # parameters
 
-data_type = ...
-style_kws = ...
-tile_layer = ...
-static = ...
-title = ...
-title_kws = ...
-scale_kws = ...
-north_arrow_kws = ...
-
-
-# %%
-# the code for Draw Ecomap
-
-'\n    Creates a map based on the provided layer definitions and configuration.\n\n    Args:\n    geodataframe (geopandas.GeoDataFrame): The geodataframe to visualize.\n    data_type (str): The type of visualization, "Scatterplot", "Path" or "Polygon".\n    style_kws (dict): Style arguments for the data visualization.\n    tile_layer (str): A named tile layer, ie OpenStreetMap.\n    static (bool): Set to true to disable map pan/zoom.\n    title (str): The map title.\n    title_kws (dict): Additional arguments for configuring the Title.\n    scale_kws (dict): Additional arguments for configuring the Scale Bar.\n    north_arrow_kws (dict): Additional arguments for configuring the North Arrow.\n\n    Returns:\n    str: A static HTML representation of the map.\n    '
-from ecoscope.mapping import EcoMap
-
-m = EcoMap(static=static, default_widgets=False)
-if title:
-    m.add_title(title, **title_kws)
-m.add_scale_bar(**scale_kws)
-m.add_north_arrow(**north_arrow_kws)
-if tile_layer:
-    m.add_layer(EcoMap.get_named_tile_layer(tile_layer))
-match data_type:
-    case "Scatterplot":
-        m.add_scatterplot_layer(geodataframe, **style_kws)
-    case "Path":
-        m.add_path_layer(geodataframe, **style_kws)
-    case "Polygon":
-        m.add_polygon_layer(geodataframe, **style_kws)
-m.zoom_to_bounds(m.layers)
+draw_ecomap_params = dict(
+    data_type=...,
+    style_kws=...,
+    tile_layer=...,
+    static=...,
+    title=...,
+    title_kws=...,
+    scale_kws=...,
+    north_arrow_kws=...,
+)
 
 # %%
-# return value from this section
+# call the task
 
-draw_ecomap_return = m.to_html()
-
-
+draw_ecomap_return = draw_ecomap(
+    geodataframe=relocations_to_trajectory_return,
+    **draw_ecomap_params,
+)
 # %% [markdown]
 # ## Persist Text
-# %%
-# dependencies assignments
-
-text = draw_ecomap_return
-
 
 # %%
 # parameters
 
-root_path = ...
-filename = ...
-
-
-# %%
-# the code for Persist Text
-
-"Persist text to a file or cloud storage object."
-from ecoscope_workflows.serde import _persist_text
+persist_text_params = dict(
+    root_path=...,
+    filename=...,
+)
 
 # %%
-# return value from this section
+# call the task
 
-persist_text_return = _persist_text(text, root_path, filename)
+persist_text_return = persist_text(
+    text=draw_ecomap_return,
+    **persist_text_params,
+)
