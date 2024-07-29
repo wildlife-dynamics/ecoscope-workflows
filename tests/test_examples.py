045b1780930dc3cc2e03357aa6619bad5a32cc67
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-29 11:36:13 -0700

    fix e2e test

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -121,8 +121,9 @@ assert_that_stdout = {
         # is different. Leaving this note here rather than deleting the commented-out asserts, bc
         # this speaks to a forthcoming item we'll need to address, namely aggregation of results
         # from non-convergent branches of the workflow.
-        lambda out: "patrol_events_ecomap.html" in out,
-        lambda out: "widget_type='map', title='Patrol Events Map'" in out,
+        lambda out: "A dashboard for visualizing patrol trajectories." in out,
+        lambda out: "patrol_traj_map.html" in out,
+        lambda out: "widget_type='map', title='Patrol Trajectory Map'" in out,
     ],
     "mode-map.yaml": [],
 }
