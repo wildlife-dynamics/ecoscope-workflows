9d63040a0bafaa1b3682049cb71510f874820185
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-07-30 11:38:41 -0700

    recompile jupytext

--- examples/dags/mode_map_dag.jupytext.py
+++ examples/dags/mode_map_dag.jupytext.py
@@ -9,7 +9,6 @@
 
 from functools import partial
 from ecoscope_workflows.tasks.io import get_subjectgroup_observations
-from ecoscope_workflows.tasks.parallel import split_groups
 from ecoscope_workflows.tasks.results import draw_ecomap
 
 # %% [markdown]
@@ -18,7 +17,7 @@ from ecoscope_workflows.tasks.results import draw_ecomap
 # %%
 # parameters
 
-obs_params = dict(
+obs_a_params = dict(
     client=...,
     subject_group_name=...,
     include_inactive=...,
@@ -29,25 +28,52 @@ obs_params = dict(
 # %%
 # call the task
 
-obs = get_subjectgroup_observations(
-    **obs_params,
+obs_a = get_subjectgroup_observations(
+    **obs_a_params,
 )
 
 # %% [markdown]
-# ## Split Groups
+# ## Get Subjectgroup Observations
 
 # %%
 # parameters
 
-split_params = dict()
+obs_b_params = dict(
+    client=...,
+    subject_group_name=...,
+    include_inactive=...,
+    since=...,
+    until=...,
+)
 
 # %%
 # call the task
 
-split = split_groups(
-    df=obs,
-    **split_params,
+obs_b = get_subjectgroup_observations(
+    **obs_b_params,
 )
+
+# %% [markdown]
+# ## Get Subjectgroup Observations
+
+# %%
+# parameters
+
+obs_c_params = dict(
+    client=...,
+    subject_group_name=...,
+    include_inactive=...,
+    since=...,
+    until=...,
+)
+
+# %%
+# call the task
+
+obs_c = get_subjectgroup_observations(
+    **obs_c_params,
+)
+
 # %% [markdown]
 # ## Draw Ecomap
 
@@ -69,5 +95,5 @@ ecomaps_params = dict(
 # call the task
 
 ecomaps_partial = partial(draw_ecomap, **ecomaps_params)
-ecomaps_mapped_iterable = map(ecomaps_partial, split)
+ecomaps_mapped_iterable = map(ecomaps_partial, [obs_a, obs_b, obs_c])
 ecomaps = list(ecomaps_mapped_iterable)
