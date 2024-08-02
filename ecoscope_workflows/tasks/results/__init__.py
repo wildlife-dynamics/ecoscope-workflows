7fb39bf1c62150109d6e35d95b11d6aca1b0e660
Alex Morling <atmorling@gmail.com>
2024-08-02 15:52:45 +0200

    update init

--- ecoscope_workflows/tasks/results/__init__.py
+++ ecoscope_workflows/tasks/results/__init__.py
@@ -1,6 +1,6 @@
 from ._dashboard import gather_dashboard
-from ._ecomap import draw_ecomap
-from ._ecoplot import draw_ecoplot
+from ._ecomap import create_map_layer, draw_ecomap
+from ._ecoplot import draw_ecoplot, draw_stacked_bar_chart, draw_pie_chart
 from ._widget_tasks import (
     create_map_widget_single_view,
     create_plot_widget_single_view,
@@ -14,8 +14,11 @@ __all__ = [
     "create_plot_widget_single_view",
     "create_text_widget_single_view",
     "create_single_value_widget_single_view",
+    "create_map_layer",
     "draw_ecomap",
     "draw_ecoplot",
+    "draw_stacked_bar_chart",
+    "draw_pie_chart",
     "gather_dashboard",
     "merge_widget_views",
 ]
