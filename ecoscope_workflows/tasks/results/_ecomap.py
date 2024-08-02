774c761a86a93ac79d1708af1090c1040fe4b33a
Alex Morling <atmorling@gmail.com>
2024-08-02 13:55:29 +0200

    tentative map updates

--- ecoscope_workflows/tasks/results/_ecomap.py
+++ ecoscope_workflows/tasks/results/_ecomap.py
@@ -7,16 +7,45 @@ from ecoscope_workflows.decorators import distributed
 
 
 @distributed
-def draw_ecomap(
+def create_map_layer(
     geodataframe: Annotated[
         AnyGeoDataFrame, Field(description="The geodataframe to visualize.")
     ],
     data_type: Annotated[
-        Literal["Scatterplot", "Path", "Polygon"],
+        Literal["Point", "Polyline", "Polygon"],
         Field(description="The type of visualization."),
     ],
-    style_kws: Annotated[
-        dict, Field(description="Style arguments for the data visualization.")
+    style_kws: Annotated[dict, Field(description="Style arguments for the layer.")],
+) -> Annotated[object, Field()]:
+    """
+    Creates a map layer based on the provided configuration.
+
+    Args:
+    geodataframe (geopandas.GeoDataFrame): The geodataframe to visualize.
+    data_type (str): The type of visualization, "Scatterplot", "Path" or "Polygon".
+    style_kws (dict): Style arguments for the data visualization.
+
+    Returns:
+    The generated map layer
+    """
+
+    from ecoscope.mapping import EcoMap
+
+    match data_type:
+        case "Point":
+            layer = EcoMap.point_layer(geodataframe, **style_kws)
+        case "Polyline":
+            layer = EcoMap.polyline_layer(geodataframe, **style_kws)
+        case "Polygon":
+            layer = EcoMap.polygon_layer(geodataframe, **style_kws)
+
+    return layer
+
+
+@distributed
+def draw_ecomap(
+    geo_layers: Annotated[
+        list[object], Field(description="A list of map layers to add to the map.")
     ],
     tile_layer: Annotated[
         str, Field(description="A named tile layer, ie OpenStreetMap.")
@@ -66,13 +95,6 @@ def draw_ecomap(
     if tile_layer:
         m.add_layer(EcoMap.get_named_tile_layer(tile_layer))
 
-    match data_type:
-        case "Scatterplot":
-            m.add_scatterplot_layer(geodataframe, **style_kws)
-        case "Path":
-            m.add_path_layer(geodataframe, **style_kws)
-        case "Polygon":
-            m.add_polygon_layer(geodataframe, **style_kws)
-
+    m.add_layer(geo_layers)
     m.zoom_to_bounds(m.layers)
-    return m.to_html()
+    return m.to_html(title=title if title is not None else "Map Export")
