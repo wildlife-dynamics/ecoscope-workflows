552f62502540991a67b408e0350170f8d76bdfeb
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-02 14:58:02 -0700

    carve out arg prevalidators & return postvalidator

--- ecoscope_workflows/decorators.py
+++ ecoscope_workflows/decorators.py
@@ -1,61 +1,19 @@
 import functools
-import types
-import warnings
 from dataclasses import FrozenInstanceError, dataclass, field, replace
 from typing import Any, Callable, Generic, TypeVar, overload
 
-import dill
 from pydantic import validate_call
-from pydantic.functional_validators import AfterValidator, BeforeValidator
 
 from ecoscope_workflows.operators import OperatorKws
 
 FuncReturn = TypeVar("FuncReturn")
-ReturnPostValidatorReturn = TypeVar("ReturnPostValidatorReturn")
-
-
-def _get_annotation_metadata(func: types.FunctionType, arg_name: str):
-    assert func.__annotations__, f"{func.__name__=} has no annotations."
-    assert (
-        arg_name in func.__annotations__
-    ), f"{arg_name=} on {func.__name__=} is not annotated."
-    assert hasattr(
-        func.__annotations__[arg_name], "__metadata__"
-    ), f"The annotation of {arg_name=} on {func.__name__=} is not of type `typing.Annotated`."
-    return list(func.__annotations__[arg_name].__metadata__)
-
-
-def _get_validator_index(
-    existing_meta: dict,
-    validator_type: AfterValidator | BeforeValidator,
-) -> int:
-    """If there are is an existing validator instance of the specified type in the metadata,
-    we will overwrite it by re-assigning to its index. if not, we will just add our new
-    validator to the end of the list (i.e., index it as -1)
-    """
-    return (
-        -1
-        if not any([isinstance(m, validator_type) for m in existing_meta])
-        else [i for i, m in enumerate(existing_meta) if isinstance(m, validator_type)][
-            0
-        ]
-    )
 
 
 @dataclass
-class DistributedTask(Generic[FuncReturn, ReturnPostValidatorReturn]):
-    """
-    Parameters
-    ----------
-    func : Callable
-        The function (or other callable) to wrap.
-    arg_prevalidators : dict[ArgName, Callable]
-        ...
-    """
+class DistributedTask(Generic[FuncReturn]):
+    """ """
 
     func: Callable[..., FuncReturn]
-    arg_prevalidators: dict[str, Callable[[str], Any]] = field(default_factory=dict)
-    return_postvalidator: Callable[..., ReturnPostValidatorReturn] | None = None
     validate: bool = False
     operator_kws: OperatorKws = field(default_factory=OperatorKws)
     tags: list[str] = field(default_factory=list)
@@ -67,16 +25,12 @@ class DistributedTask(Generic[FuncReturn, ReturnPostValidatorReturn]):
 
     def replace(
         self,
-        arg_prevalidators: dict[str, Callable] | None = None,
-        return_postvalidator: Callable | None = None,
         validate: bool | None = None,
-    ) -> "DistributedTask[FuncReturn, ReturnPostValidatorReturn]":
+    ) -> "DistributedTask[FuncReturn]":
         self._initialized = False
         changes = {
             k: v
             for k, v in {
-                "arg_prevalidators": arg_prevalidators,
-                "return_postvalidator": return_postvalidator,
                 "validate": validate,
             }.items()
             if v is not None
@@ -91,39 +45,11 @@ class DistributedTask(Generic[FuncReturn, ReturnPostValidatorReturn]):
             )
         return super().__setattr__(name, value)
 
-    def __call__(self, *args, **kwargs) -> FuncReturn | ReturnPostValidatorReturn:
-        # to get distributed behaviors, we need to mutate the __annotations__ dict of
-        # the function before calling it. if we don't make a deep copy, these changes
-        # will effect future calls. this is a way to make a copy that doesn't share a
-        # reference to the original __annotations__ dict. perhaps a function factory
-        # would be a more robust approach.
-        func_copy = dill.loads(dill.dumps(self.func))
-        # TODO: make sure the keys of arg_prevalidators are all arg_names on self.func
-        for arg_name in self.arg_prevalidators:
-            arg_meta = _get_annotation_metadata(func_copy, arg_name)
-            bv_idx = _get_validator_index(arg_meta, validator_type=BeforeValidator)
-            arg_meta[bv_idx] = BeforeValidator(func=self.arg_prevalidators[arg_name])
-            func_copy.__annotations__[arg_name].__metadata__ = tuple(arg_meta)
-        # TODO: make sure return_postvalidator is a single-argument callable
-        if self.return_postvalidator:
-            return_meta = _get_annotation_metadata(func_copy, "return")
-            av_idx = _get_validator_index(return_meta, validator_type=AfterValidator)
-            return_meta[av_idx] = AfterValidator(func=self.return_postvalidator)
-            func_copy.__annotations__["return"].__metadata__ = tuple(return_meta)
-        # TODO: If `ecoscope.distributed.types.DataFrame` is used as an arg or return type,
-        # and it is *not* subscripted with a pandera schema, we get a cryptic error from
-        # pydantic under validate=True contexts... make this error more descriptive or catch
-        # it somewhere in this decorator before making this call and confusing everyone.
-        if (self.arg_prevalidators or self.return_postvalidator) and not self.validate:
-            warnings.warn(
-                f"`@distributed`-decorated function has `{self.arg_prevalidators=}` "
-                f"and  `{self.return_postvalidator=}` but `{self.validate=}`. Pre- "
-                "and post- call behavior is only modified when `self.validate=True`."
-            )
+    def __call__(self, *args, **kwargs) -> FuncReturn:
         return (
-            validate_call(func_copy, validate_return=True)(*args, **kwargs)
+            validate_call(self.func, validate_return=True)(*args, **kwargs)
             if self.validate
-            else func_copy(*args, **kwargs)
+            else self.func(*args, **kwargs)
         )
 
 
@@ -134,7 +60,7 @@ def distributed(
     image: str | None = None,
     container_resources: dict[str, Any] | None = None,
     tags: list[str] | None = None,
-) -> DistributedTask[FuncReturn, ReturnPostValidatorReturn]: ...
+) -> DistributedTask[FuncReturn]: ...
 
 
 @overload  # @distributed(...) style
@@ -145,7 +71,7 @@ def distributed(
     tags: list[str] | None = None,
 ) -> Callable[
     [Callable[..., FuncReturn]],
-    DistributedTask[FuncReturn, ReturnPostValidatorReturn],
+    DistributedTask[FuncReturn],
 ]: ...
 
 
@@ -155,10 +81,7 @@ def distributed(
     image: str | None = None,
     container_resources: dict[str, Any] | None = None,
     tags: list[str] | None = None,
-) -> (
-    Callable[..., DistributedTask[FuncReturn, ReturnPostValidatorReturn]]
-    | DistributedTask[FuncReturn, ReturnPostValidatorReturn]
-):
+) -> Callable[..., DistributedTask[FuncReturn]] | DistributedTask[FuncReturn]:
     operator_kws = {
         k: v
         for k, v in {"image": image, "container_resources": container_resources}.items()
@@ -167,7 +90,7 @@ def distributed(
 
     def wrapper(
         func: Callable[..., FuncReturn],
-    ) -> DistributedTask[FuncReturn, ReturnPostValidatorReturn]:
+    ) -> DistributedTask[FuncReturn]:
         return DistributedTask(
             func,
             operator_kws=OperatorKws(**operator_kws),
