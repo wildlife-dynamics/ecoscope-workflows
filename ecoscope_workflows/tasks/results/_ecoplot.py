0b9ecc2e7ab343787ef6d6ee91c814a2561f2dbe
Alex Morling <atmorling@gmail.com>
2024-08-02 10:40:33 +0200

    first pass at bar chart task

--- ecoscope_workflows/tasks/results/_ecoplot.py
+++ ecoscope_workflows/tasks/results/_ecoplot.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Literal
 
 from pydantic import Field
 
@@ -47,6 +47,86 @@ def draw_ecoplot(
 
     plot = plotting.ecoplot(
         data=[data],
+    )
+
+    return plot.to_html(
+        default_height="100%",
+        default_width="100%",
+        config={
+            "autosizable": True,
+            "fillFrame": True,
+            "responsive": True,
+            "displayModeBar": False,
+        },
+    )
+
+
+@distributed
+def draw_stacked_bar_chart(
+    dataframe: DataFrame[JsonSerializableDataFrameModel],
+    x_axis: Annotated[
+        str, Field(description="The dataframe column to plot in the x axis.")
+    ],
+    y_axis: Annotated[
+        str, Field(description="The dataframe column to plot in the y axis.")
+    ],
+    stack_column: Annotated[
+        str, Field(description="The dataframe column to stack in the y axis.")
+    ],
+    agg_function: Annotated[
+        Literal["count", "mean", "sum", "min", "max"],
+        Field(description="The aggregate function to apply to the group."),
+    ],
+    groupby_style_kws: Annotated[
+        dict | None,
+        Field(
+            description="Style arguments passed to plotly.graph_objects.Bar and applied to individual groups."
+        ),
+    ],
+    style_kws: Annotated[
+        dict | None,
+        Field(
+            description="Style arguments passed to plotly.graph_objects.Bar and applied to all groups."
+        ),
+    ],
+    layout_kws: Annotated[
+        dict | None,
+        Field(description="Style arguments passed to plotly.graph_objects.Figure."),
+    ],
+) -> Annotated[str, Field()]:
+    """
+    Generates a stacked bar chart from the provided params
+
+    Args:
+    dataframe (pd.DataFrame): The input dataframe.
+    x_axis (str): The dataframe column to plot in the x axis.
+    y_axis (str): The dataframe column to plot in the y axis.
+    stack_column (str): The dataframe column to stack in the y axis.
+    agg_function (str): The aggregate function to apply to the group.
+    groupby_style_kws (dict): Style arguments passed to plotly.graph_objects.Bar and applied to individual groups.
+    style_kws (dict): Style arguments passed to plotly.graph_objects.Bar and applied to all groups.
+    layout_kws (dict): Style arguments passed to plotly.graph_objects.Figure.
+
+    Returns:
+    The generated plot html as a string
+    """
+    from ecoscope.plotting import stacked_bar_chart, EcoPlotData
+
+    grouped = dataframe.groupby([x_axis, stack_column])
+
+    data = EcoPlotData(
+        grouped=grouped,
+        x_col=x_axis,
+        y_col=y_axis,
+        groupby_style=groupby_style_kws,
+        **style_kws,
+    )
+
+    plot = stacked_bar_chart(
+        data=data,
+        agg_function=agg_function,
+        stack_column=stack_column,
+        layout_kwargs=layout_kws,
     )
 
     return plot.to_html(
--- tests/tasks/test_ecoplot.py
+++ tests/tasks/test_ecoplot.py
@@ -1,7 +1,10 @@
 import pytest
 import pandas as pd
 
-from ecoscope_workflows.tasks.results._ecoplot import draw_ecoplot
+from ecoscope_workflows.tasks.results._ecoplot import (
+    draw_ecoplot,
+    draw_stacked_bar_chart,
+)
 
 
 @pytest.fixture
@@ -22,6 +25,24 @@ def sample_dataframe():
     return pd.DataFrame(data)
 
 
+@pytest.fixture
+def stacked_dataframe():
+    """Fixture to provide a sample DataFrame for testing."""
+    data = {
+        "value": [500, 200, 300, 150, 400],
+        "category": ["A", "B", "A", "B", "B"],
+        "time": [
+            pd.to_datetime("2024-06-01", utc=True),
+            pd.to_datetime("2024-06-01", utc=True),
+            pd.to_datetime("2024-06-02", utc=True),
+            pd.to_datetime("2024-06-02", utc=True),
+            pd.to_datetime("2024-06-02", utc=True),
+        ],
+    }
+
+    return pd.DataFrame(data)
+
+
 def test_draw_ecoplot(sample_dataframe):
     groupby = "category"
     x_axis = "time"
@@ -33,6 +54,21 @@ def test_draw_ecoplot(sample_dataframe):
         x_axis=x_axis,
         y_axis=y_axis,
         style_kws={"line": {"color": "green"}},
+    )
+
+    assert isinstance(plot, str)
+
+
+def test_draw_stacked_bar_chart(stacked_dataframe):
+    plot = draw_stacked_bar_chart(
+        stacked_dataframe,
+        x_axis="time",
+        y_axis="category",
+        stack_column="category",
+        agg_function="count",
+        groupby_style_kws={"A": {"marker_color": "red"}, "B": {"marker_color": "blue"}},
+        style_kws={"xperiodalignment": "middle"},
+        layout_kws={"xaxis_dtick": 86400000},
     )
 
     assert isinstance(plot, str)
