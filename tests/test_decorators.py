8a49905a1c6ec971f3e97933ac2b079940da330a
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-05 14:39:01 -0700

    simple decorator tests

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -16,3 +16,35 @@ def test_frozen_instance():
 
     f_new = f.replace(validate=True)
     assert f_new.validate
+
+
+def test_call_simple():
+    @task
+    def f(a: int) -> int:
+        return a
+
+    assert f(1) == 1
+    assert f(2) == 2
+
+
+def test_call_alias_simple():
+    @task
+    def f(a: int) -> int:
+        return a
+
+    assert f.call(1) == 1
+    assert f.call(2) == 2
+
+
+def test_map_simple():
+    @task
+    def f(a: int) -> int:
+        return a
+
+    assert f.map([1, 2, 3]) == [1, 2, 3]
+
+    @task
+    def double(a: int) -> int:
+        return a * 2
+
+    assert double.map([1, 2, 3]) == [2, 4, 6]
