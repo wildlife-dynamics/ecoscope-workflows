1e8ecc4e026afe3b31a59859a8d17a2c9752c56e
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-05 16:19:09 -0700

    lithops executor placeholder

---
+++ ecoscope_workflows/executors/lithops.py
@@ -0,0 +1,25 @@
+from typing import Callable, ParamSpec, Iterable, Sequence, TypeVar
+
+from .base import Executor
+
+P = ParamSpec("P")
+R = TypeVar("R")
+K = TypeVar("K")
+V = TypeVar("V")
+
+
+class LithopsExecutor(Executor[P, R, K, V]):
+    def call(self, func: Callable[P, R], *args: P.args, **kwargs: P.kwargs) -> R:
+        return func(*args, **kwargs)
+
+    def map(
+        self, func: Callable[..., R], iterable: Iterable[dict[str, V]]
+    ) -> Sequence[R]:
+        raise NotImplementedError
+
+    def mapvalues(
+        self,
+        func: Callable[..., tuple[K, R]],
+        iterable: Iterable[tuple[K, dict[str, V]]],
+    ) -> Sequence[tuple[K, R]]:
+        raise NotImplementedError
