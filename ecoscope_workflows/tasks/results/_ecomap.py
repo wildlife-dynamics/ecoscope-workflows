209ae878593efe2d75ac73d829dab3e160025eb2
Yun Wu <ywu910318@gmail.com>
2024-08-12 15:09:26 +0800

    Define layer styles

--- ecoscope_workflows/tasks/results/_ecomap.py
+++ ecoscope_workflows/tasks/results/_ecomap.py
@@ -1,37 +1,62 @@
 from dataclasses import dataclass
-from typing import Annotated, Literal
+from typing import Annotated
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 from pydantic.json_schema import SkipJsonSchema
 
 from ecoscope_workflows.annotations import AnyGeoDataFrame
 from ecoscope_workflows.decorators import task
 
 
+class LayerStyleProperty(BaseModel):
+    auto_highlight: bool = False
+    opacity: float = 1
+    pickable: bool = True
+
+
+class PolylineLayerProperty(LayerStyleProperty):
+    get_color: list[int] = [0, 0, 0, 255]
+    get_width: float = 1
+
+
+class ShapeLayerProperty(LayerStyleProperty):
+    filled: bool = True
+    get_fill_color: list[int] = [0, 0, 0, 255]
+    get_line_color: list[int] = [0, 0, 0, 255]
+    get_line_width: float = 1
+
+
+class PointLayerProperty(ShapeLayerProperty):
+    get_radius: float = 1
+
+
+class PolygonLayerProperty(ShapeLayerProperty):
+    extruded: bool = False
+    get_elevation: float = 1000
+
+
 @dataclass
 class LayerDefinition:
     geodataframe: AnyGeoDataFrame
-    data_type: str
-    style_kws: dict
+    style_kws: LayerStyleProperty
 
 
 @task
 def create_map_layer(
     geodataframe: Annotated[
-        AnyGeoDataFrame, Field(description="The geodataframe to visualize.")
+        AnyGeoDataFrame,
+        Field(description="The geodataframe to visualize.", exclude=True),
     ],
-    data_type: Annotated[
-        Literal["Point", "Polyline", "Polygon"],
-        Field(description="The type of visualization."),
+    layer_style: Annotated[
+        PolylineLayerProperty | PolygonLayerProperty | PointLayerProperty,
+        Field(description="Style arguments for the layer."),
     ],
-    style_kws: Annotated[dict, Field(description="Style arguments for the layer.")],
 ) -> Annotated[LayerDefinition, Field()]:
     """
     Creates a map layer definition based on the provided configuration.
 
     Args:
     geodataframe (geopandas.GeoDataFrame): The geodataframe to visualize.
-    data_type (str): The type of visualization, "Scatterplot", "Path" or "Polygon".
     style_kws (dict): Style arguments for the data visualization.
 
     Returns:
@@ -40,8 +65,7 @@ def create_map_layer(
 
     return LayerDefinition(
         geodataframe=geodataframe,
-        data_type=data_type,
-        style_kws=style_kws,
+        style_kws=layer_style,
     )
 
 
@@ -49,7 +73,7 @@ def create_map_layer(
 def draw_ecomap(
     geo_layers: Annotated[
         LayerDefinition | list[LayerDefinition],
-        Field(description="A list of map layers to add to the map."),
+        Field(description="A list of map layers to add to the map.", exclude=True),
     ],
     tile_layer: Annotated[
         str, Field(description="A named tile layer, ie OpenStreetMap.")
@@ -104,19 +128,19 @@ def draw_ecomap(
 
     geo_layers = [geo_layers] if not isinstance(geo_layers, list) else geo_layers
     for layer_def in geo_layers:
-        match layer_def.data_type:
-            case "Point":
-                layer = EcoMap.point_layer(
-                    layer_def.geodataframe, **layer_def.style_kws
-                )
-            case "Polyline":
-                layer = EcoMap.polyline_layer(
-                    layer_def.geodataframe, **layer_def.style_kws
-                )
-            case "Polygon":
-                layer = EcoMap.polygon_layer(
-                    layer_def.geodataframe, **layer_def.style_kws
-                )
+        if isinstance(layer_def.style_kws, PolylineLayerProperty):
+            layer = EcoMap.polyline_layer(
+                layer_def.geodataframe, **layer_def.style_kws.model_dump()
+            )
+        elif isinstance(layer_def.style_kws, PointLayerProperty):
+            layer = EcoMap.point_layer(
+                layer_def.geodataframe, **layer_def.style_kws.model_dump()
+            )
+        elif isinstance(layer_def, PolygonLayerProperty):
+            layer = EcoMap.polygon_layer(
+                layer_def.geodataframe, **layer_def.style_kws.model_dump()
+            )
+
         m.add_layer(layer)
 
     m.zoom_to_bounds(m.layers)
