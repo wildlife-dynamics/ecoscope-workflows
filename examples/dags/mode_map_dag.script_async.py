1fd7fdb4f7b12cd9bfab6214de8668728a461598
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-15 13:02:57 -0700

    async script prototype WIP

--- examples/dags/mode_map_dag.script_async.py
+++ examples/dags/mode_map_dag.script_async.py
@@ -1,11 +1,10 @@
 import argparse
 import os
-from dataclasses import dataclass
-from typing import Any, Callable
 
 import yaml
 
 from ecoscope_workflows.executors import LithopsExecutor
+from ecoscope_workflows.graph import DependsOn, Graph, Node
 from ecoscope_workflows.tasks.io import get_subjectgroup_observations
 from ecoscope_workflows.tasks.results import create_map_layer
 from ecoscope_workflows.tasks.results import draw_ecomap
@@ -25,72 +24,66 @@ if __name__ == "__main__":
 
     le = LithopsExecutor()
 
-    @dataclass
-    class Node:
-        callable: Callable
-        params: dict[str, Any]
-
-    @dataclass
-    class NodeResult:
-        result: Any
-
-    node_results = {
-        "obs_a": NodeResult(
-            get_subjectgroup_observations.validate()
-            .set_executor(le)
-            .call(**params["obs_a"]),
-        ),
-        "obs_b": NodeResult(
-            get_subjectgroup_observations.validate()
-            .set_executor(le)
-            .call(**params["obs_b"]),
-        ),
-        "obs_c": NodeResult(
-            get_subjectgroup_observations.validate()
-            .set_executor(le)
-            .call(**params["obs_c"]),
-        ),
-    }
     nodes = {
         "obs_a": Node(
-            get_subjectgroup_observations.validate().set_executor(le),
-            params["obs_a"],
+            async_callable=get_subjectgroup_observations.validate().set_executor(le),
+            params=params["obs_a"],
         ),
         "obs_b": Node(
-            get_subjectgroup_observations.validate().set_executor(le),
-            params["obs_b"],
+            async_callable=get_subjectgroup_observations.validate().set_executor(le),
+            params=params["obs_b"],
         ),
         "obs_c": Node(
-            get_subjectgroup_observations.validate().set_executor(le),
-            params["obs_c"],
+            async_callable=get_subjectgroup_observations.validate().set_executor(le),
+            params=params["obs_c"],
         ),
         "map_layers": Node(
-            create_map_layer.validate()
-            .partial(**params["map_layers"])
-            .set_executor(le),
-            {"argnames": ["geodataframe"], "argvalues": [obs_a, obs_b, obs_c]},
+            async_callable=(
+                create_map_layer.validate()
+                .partial(**params["map_layers"])
+                .set_executor(le)
+            ),
+            params={
+                "argnames": ["geodataframe"],
+                "argvalues": [
+                    DependsOn("obs_a"),
+                    DependsOn("obs_b"),
+                    DependsOn("obs_c"),
+                ],
+            },
+        ),
+        "ecomaps": Node(
+            async_callable=(
+                draw_ecomap.validate().partial(**params["ecomaps"]).set_executor(le)
+            ),
+            params={
+                "argnames": ["geo_layers"],
+                "argvalues": [DependsOn("map_layers")],
+            },
+        ),
+        "td_ecomap_html_url": Node(
+            async_callable=(
+                persist_text.validate()
+                .partial(
+                    root_path=os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
+                    **params["td_ecomap_html_url"],
+                )
+                .set_executor(le)
+            ),
+            params={
+                "argnames": ["text"],
+                "argvalues": [DependsOn("ecomaps")],
+            },
         ),
     }
-
-    map_layers = (
-        create_map_layer.validate()
-        .partial(**params["map_layers"])
-        .map(argnames=["geodataframe"], argvalues=[obs_a, obs_b, obs_c])
-    )
-
-    ecomaps = (
-        draw_ecomap.validate()
-        .partial(**params["ecomaps"])
-        .map(argnames=["geo_layers"], argvalues=map_layers)
-    )
-
-    td_ecomap_html_url = (
-        persist_text.validate()
-        .partial(
-            root_path=os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
-            **params["td_ecomap_html_url"],
-        )
-        .map(argnames=["text"], argvalues=ecomaps)
-    )
-
-    print(td_ecomap_html_url)
+    dependencies = {
+        "obs_a": [],
+        "obs_b": [],
+        "obs_c": [],
+        "map_layers": ["obs_a", "obs_b", "obs_c"],
+        "ecomaps": ["map_layers"],
+        "td_ecomap_html_url": ["ecomaps"],
+    }
+    graph = Graph(dependencies=dependencies, nodes=nodes)
+    results = graph.execute()
+    print(results)
