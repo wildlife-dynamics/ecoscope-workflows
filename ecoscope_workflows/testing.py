17b8a126da2bdfa87268e7d002be86cd751ff607
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-19 22:49:34 -0700

    dramatically simplify create_task_magicmock

--- ecoscope_workflows/testing.py
+++ ecoscope_workflows/testing.py
@@ -1,47 +1,20 @@
-from typing import Protocol
 from unittest.mock import MagicMock, create_autospec
 
+from ecoscope_workflows.decorators import SyncTask
 from ecoscope_workflows.util import (
     load_example_return_from_task_reference,
     import_task_from_reference,
 )
 
 
-class MockWrappedFunctionProtocol(Protocol):
-    return_value: MagicMock
-
-    def __call__(self, *args, **kws):
-        """Mocks the call signature of a function decorated by `@task`.
-        These functions can have arbitrary signatures, so we use `*args` and `**kws`.
-        """
-        ...
-
-
-class MockReturnsMutatedTaskCopyProtocol(Protocol):
-    return_value: "TaskMagicMock"
-
-    def __call__(self) -> "TaskMagicMock": ...
-
-    def call(self, *args, **kws) -> "TaskMagicMock": ...
-
-
-class TaskMagicMock(MagicMock):
-    validate: MockReturnsMutatedTaskCopyProtocol
-    set_executor: MockReturnsMutatedTaskCopyProtocol
-    call: MockWrappedFunctionProtocol
-
-
-def create_task_magicmock(anchor: str, func_name: str) -> TaskMagicMock:
+def create_task_magicmock(anchor: str, func_name: str) -> SyncTask:
     task = import_task_from_reference(anchor, func_name)
-    # the base `task`
-    mock_task_0: TaskMagicMock = create_autospec(spec=task)
-    # the new task returned by `task.validate()`
-    mock_task_1: TaskMagicMock = create_autospec(spec=task)
-    mock_task_0.validate.return_value = mock_task_1
-    # setup the signature of `task.validate().call`
-    mock_task_1.call = create_autospec(spec=task.func)
-    # assign the return value of `task.validate().call()`
+    mock_func: MagicMock = create_autospec(spec=task.func)
     example_return = load_example_return_from_task_reference(anchor, func_name)
-    mock_task_1.call.return_value = example_return
-    # return the base `task`
-    return mock_task_0
+    mock_func.return_value = example_return
+
+    class MockSyncTask(SyncTask):
+        def validate(self):
+            return self
+
+    return MockSyncTask(func=mock_func, tags=task.tags, executor=task.executor)
