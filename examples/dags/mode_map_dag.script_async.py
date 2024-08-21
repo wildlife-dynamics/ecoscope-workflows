1edc9d55cd4783876bdb4aa11a5727b161dc7894
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-21 14:41:20 -0700

    actually compile graph script

--- examples/dags/mode_map_dag.script_async.py
+++ examples/dags/mode_map_dag.script_async.py
@@ -1,14 +1,14 @@
 import argparse
 import os
-
 import yaml
 
 from ecoscope_workflows.executors import LithopsExecutor
 from ecoscope_workflows.graph import DependsOn, DependsOnSequence, Graph, Node
+
+from ecoscope_workflows.tasks.io import get_subjectgroup_observations
 from ecoscope_workflows.tasks.results import create_map_layer
 from ecoscope_workflows.tasks.results import draw_ecomap
 from ecoscope_workflows.tasks.io import persist_text
-from ecoscope_workflows.tasks.io import get_subjectgroup_observations
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
