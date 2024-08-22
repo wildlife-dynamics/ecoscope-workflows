0f495724c44ff7b7b2ef3c130bf7cbd8c26404fa
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-08-21 22:31:52 -0700

    in graph, don't re-gather already gathered results

--- ecoscope_workflows/graph.py
+++ ecoscope_workflows/graph.py
@@ -1,9 +1,12 @@
+import logging
 from dataclasses import dataclass, field
 from graphlib import TopologicalSorter
 from typing import Any, Literal
 
 from ecoscope_workflows.decorators import AsyncTask
 from ecoscope_workflows.executors import Future, FutureSequence
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
@@ -29,24 +32,39 @@ class Node:
 Dependencies = dict[str, list[str]]  # TODO: `set` instead of `list`
 Nodes = dict[str, Node]
 FuturesDict = dict[str, Future | FutureSequence]
-Resolved = Any | list[Any]
+Gathered = Any | list[Any]
+GatheredDict = dict[str, Gathered]
+GlobalGatheredDict = GatheredDict
 
 
-def hydrate_dependencies(
-    kwargs: dict[str, Resolved | Dependency],
+def gather_dependencies(
+    kwargs: dict[str, Gathered | Dependency],
     futures: FuturesDict,
-) -> dict[str, Resolved]:
-    hydrated_kwargs = {}
+    global_gathered_dict: GatheredDict,
+) -> tuple[GatheredDict, GlobalGatheredDict]:
+    gathered_dict = {}
     for k, v in kwargs.items():
         match v:
             case DependsOn():
-                resolved = futures[v.node_name].gather()
+                if v.node_name in global_gathered_dict:
+                    gathered = global_gathered_dict[v.node_name]
+                else:
+                    gathered = futures[v.node_name].gather()
+                    global_gathered_dict[v.node_name] = gathered
             case DependsOnSequence():
-                resolved = [futures[x.node_name].gather() for x in v]
+                gathered = []
+                for x in v:
+                    if x.node_name in global_gathered_dict:
+                        gathered.append(global_gathered_dict[x.node_name])
+                    else:
+                        x_gathered = futures[x.node_name].gather()
+                        gathered.append(x_gathered)
+                        global_gathered_dict[x.node_name] = x_gathered
             case _:
-                resolved = v
-        hydrated_kwargs[k] = resolved
-    return hydrated_kwargs
+                gathered = v
+        gathered_dict[k] = gathered
+
+    return gathered_dict, global_gathered_dict
 
 
 @dataclass
@@ -60,12 +78,22 @@ class Graph:
         ts = TopologicalSorter(self.dependencies)
         ts.prepare()
         futures: FuturesDict = {}
+        global_gathered_dict: GlobalGatheredDict = {}
         while ts.is_active():
             ready = [name for name in ts.get_ready()]
-            for name in ready:
+            for name in sorted(ready):
+                logger.info(f"Executing node: '{name}'")
                 node = self.nodes[name]
-                hydrated_kwargs = hydrate_dependencies(node.kwargs, futures)
-                hydrated_partial = hydrate_dependencies(node.partial, futures)
+                hydrated_kwargs, global_gathered_dict = gather_dependencies(
+                    node.kwargs,
+                    futures,
+                    global_gathered_dict,
+                )
+                hydrated_partial, global_gathered_dict = gather_dependencies(
+                    node.partial,
+                    futures,
+                    global_gathered_dict,
+                )
                 partial = getattr(node.async_task, "partial")(**hydrated_partial)
                 callable_method = getattr(partial, node.method)
                 future = callable_method(**hydrated_kwargs)
