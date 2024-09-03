9ceb2dd30abcdcc52ed1672f031bf7d7403f6169
Alex Morling <atmorling@gmail.com>
2024-09-03 13:24:19 +0200

    add apply_classification task

--- ecoscope_workflows/tasks/transformation/__init__.py
+++ ecoscope_workflows/tasks/transformation/__init__.py
@@ -1,13 +1,15 @@
 from ._exploding import explode
 from ._filtering import apply_reloc_coord_filter
 from ._indexing import add_temporal_index
-from ._mapping import color_map, map_columns, map_values
+from ._mapping import map_columns, map_values
+from ._classification import apply_classification, apply_color_map
 
 __all__ = [
     "add_temporal_index",
     "map_columns",
-    "color_map",
     "map_values",
     "apply_reloc_coord_filter",
     "explode",
+    "apply_color_map",
+    "apply_classification",
 ]
--- tests/tasks/test_classification.py
+++ tests/tasks/test_classification.py
@@ -1,13 +1,26 @@
 import matplotlib
 import numpy as np
 import pandas as pd
+import pytest
 
-from ecoscope_workflows.tasks.transformation import color_map
+from ecoscope_workflows.tasks.transformation._classification import (
+    apply_classification,
+    apply_color_map,
+    SharedArgs,
+    MaxBreaksArgs,
+    StdMeanArgs,
+    NaturalBreaksArgs,
+)
+
+
+@pytest.fixture
+def test_df():
+    return pd.DataFrame({"column_name": [5, 3, 1, 6, 5, 9]})
 
 
 def test_color_map():
     df = pd.DataFrame({"column_name": ["A", "B", "A", "C", "B", "C"]})
-    result = color_map(df, "column_name", "viridis")
+    result = apply_color_map(df, "column_name", "viridis")
 
     assert "label" in result.columns
 
@@ -17,3 +30,24 @@ def test_color_map():
             row["label"],
             matplotlib.colormaps["viridis"](color_mapping[row["column_name"]]),
         )
+
+
+@pytest.mark.parametrize(
+    "scheme, args",
+    [
+        ("equal_interval", SharedArgs(k=3)),
+        ("max_breaks", MaxBreaksArgs(k=4, min_diff=20)),
+        ("natural_breaks", NaturalBreaksArgs(k=4, initial=3)),
+        ("std_mean", StdMeanArgs(multiples=[-1, 1], anchor=False)),
+    ],
+)
+def test_apply_classification_equal_interval(test_df, scheme, args):
+    result = apply_classification(
+        df=test_df,
+        input_column_name="column_name",
+        scheme=scheme,
+        output_column_name="classified",
+        classification_options=args,
+    )
+
+    assert "classified" in result.columns
