7da3bebbab852fcf64c279c8870635e2b5927fdb
Alex Morling <atmorling@gmail.com>
2024-09-04 20:35:10 +0200

    grid_to_raster updates

--- ecoscope_workflows/tasks/results/_raster.py
+++ ecoscope_workflows/tasks/results/_raster.py
@@ -9,7 +9,7 @@ from ecoscope_workflows.decorators import task
 
 @dataclass
 class RasterObject:
-    raster: Any
+    image: Any
 
 
 @task
@@ -21,12 +21,6 @@ def grid_to_raster(
     value_column: Annotated[
         str, Field(description="The dataframe column with values to rasterize.")
     ] = "",
-    cell_width: Annotated[
-        int, Field(description="The width of the raster cells in meters.")
-    ] = 5000,
-    cell_height: Annotated[
-        int, Field(description="The height of the raster cells in meters.")
-    ] = 5000,
 ) -> RasterObject:
     """
     Save a GeoDataFrame grid to a raster.
@@ -43,10 +37,8 @@ def grid_to_raster(
     from ecoscope.io.raster import grid_to_raster
 
     return RasterObject(
-        raster=grid_to_raster(
+        image=grid_to_raster(
             grid=geodataframe,
             val_column=value_column,
-            xlen=cell_width,
-            ylen=cell_height,
         )
     )
--- tests/tasks/test_raster.py
+++ tests/tasks/test_raster.py
@@ -1,5 +1,4 @@
 import random
-
 import geopandas as gpd
 
 from ecoscope_workflows.tasks.analysis import create_meshgrid
@@ -11,19 +10,13 @@ def test_grid_to_raster():
     bounds = random_3857_rectangle(200000, 200000, 200000, 200000, utm_safe=True)
     aoi = gpd.GeoDataFrame(geometry=[bounds], crs="EPSG:3857")
 
-    cell_width = 100000
-    cell_height = 100000
+    cell_width = 10000
+    cell_height = 10000
 
     grid = create_meshgrid(
         aoi, cell_width=cell_width, cell_height=cell_height, intersecting_only=False
     )
     grid["fake_density"] = grid.apply(lambda _: random.randint(1, 50), axis=1)
 
-    raster = grid_to_raster(
-        grid,
-        value_column="fake_density",
-        cell_width=cell_width,
-        cell_height=cell_height,
-    )
-
+    raster = grid_to_raster(grid, value_column="fake_density")
     assert raster is not None
