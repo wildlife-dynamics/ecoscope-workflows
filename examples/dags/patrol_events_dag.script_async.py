26f1522250036a69a0674438e852d7563a9b0a41
Alex Morling <atmorling@gmail.com>
2024-09-05 15:20:53 +0200

    workflow updates

---
+++ examples/dags/patrol_events_dag.script_async.py
@@ -0,0 +1,410 @@
+import argparse
+import os
+import yaml
+
+from ecoscope_workflows.graph import DependsOn, DependsOnSequence, Graph, Node
+
+from ecoscope_workflows.tasks.groupby import set_groupers
+from ecoscope_workflows.tasks.io import get_patrol_events
+from ecoscope_workflows.tasks.transformation import apply_reloc_coord_filter
+from ecoscope_workflows.tasks.transformation import add_temporal_index
+from ecoscope_workflows.tasks.transformation import apply_classification
+from ecoscope_workflows.tasks.transformation import apply_color_map
+from ecoscope_workflows.tasks.results import create_map_layer
+from ecoscope_workflows.tasks.results import draw_ecomap
+from ecoscope_workflows.tasks.io import persist_text
+from ecoscope_workflows.tasks.results import create_map_widget_single_view
+from ecoscope_workflows.tasks.results import draw_time_series_bar_chart
+from ecoscope_workflows.tasks.results import create_plot_widget_single_view
+from ecoscope_workflows.tasks.analysis import create_meshgrid
+from ecoscope_workflows.tasks.analysis import calculate_feature_density
+from ecoscope_workflows.tasks.groupby import split_groups
+from ecoscope_workflows.tasks.results import draw_pie_chart
+from ecoscope_workflows.tasks.results import merge_widget_views
+from ecoscope_workflows.tasks.results import gather_dashboard
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    g = parser.add_argument_group("patrol_events_workflow")
+    g.add_argument(
+        "--config-file",
+        dest="config_file",
+        required=True,
+        type=argparse.FileType(mode="r"),
+    )
+    args = parser.parse_args()
+    params = yaml.safe_load(args.config_file)
+
+    dependencies = {
+        "groupers": [],
+        "patrol_events": [],
+        "filter_patrol_events": ["patrol_events"],
+        "pe_add_temporal_index": ["filter_patrol_events"],
+        "pe_classify": ["filter_patrol_events"],
+        "pe_colormap": ["pe_classify"],
+        "pe_map_layer": ["pe_colormap"],
+        "pe_ecomap": ["pe_map_layer"],
+        "pe_ecomap_html_url": ["pe_ecomap"],
+        "pe_map_widget": ["pe_ecomap_html_url"],
+        "pe_bar_chart": ["filter_patrol_events"],
+        "pe_bar_chart_html_url": ["pe_bar_chart"],
+        "pe_bar_chart_widget": ["pe_bar_chart_html_url"],
+        "pe_meshgrid": ["filter_patrol_events"],
+        "pe_feature_density": ["filter_patrol_events", "pe_meshgrid"],
+        "fd_map_layer": ["pe_feature_density"],
+        "fd_ecomap": ["fd_map_layer"],
+        "fd_ecomap_html_url": ["fd_ecomap"],
+        "fd_map_widget": ["fd_ecomap_html_url"],
+        "split_patrol_event_groups": ["pe_add_temporal_index", "groupers"],
+        "grouped_pe_classify": ["split_patrol_event_groups"],
+        "grouped_pe_colormap": ["grouped_pe_classify"],
+        "grouped_pe_map_layer": ["grouped_pe_colormap"],
+        "grouped_pe_ecomap": ["grouped_pe_map_layer"],
+        "grouped_pe_ecomap_html_url": ["grouped_pe_ecomap"],
+        "grouped_pe_map_widget": ["grouped_pe_ecomap_html_url"],
+        "grouped_pe_pie_chart": ["split_patrol_event_groups"],
+        "grouped_pe_pie_chart_html_urls": ["grouped_pe_pie_chart"],
+        "grouped_pe_pie_chart_widgets": ["grouped_pe_pie_chart_html_urls"],
+        "grouped_pe_pie_widget_grouped": ["grouped_pe_pie_chart_widgets"],
+        "grouped_pe_feature_density": ["pe_meshgrid", "split_patrol_event_groups"],
+        "grouped_fd_map_layer": ["pe_feature_density"],
+        "grouped_fd_ecomap": ["grouped_fd_map_layer"],
+        "grouped_fd_ecomap_html_url": ["grouped_fd_ecomap"],
+        "grouped_fd_map_widget": ["grouped_fd_ecomap_html_url"],
+        "patrol_dashboard": [
+            "pe_map_widget",
+            "pe_bar_chart_widget",
+            "fd_map_widget",
+            "grouped_pe_map_widget",
+            "grouped_pe_pie_widget_grouped",
+            "grouped_fd_map_widget",
+            "groupers",
+        ],
+    }
+
+    nodes = {
+        "groupers": Node(
+            async_task=set_groupers.validate().set_executor("lithops"),
+            partial=params["groupers"],
+            method="call",
+        ),
+        "patrol_events": Node(
+            async_task=get_patrol_events.validate().set_executor("lithops"),
+            partial=params["patrol_events"],
+            method="call",
+        ),
+        "filter_patrol_events": Node(
+            async_task=apply_reloc_coord_filter.validate().set_executor("lithops"),
+            partial={
+                "df": DependsOn("patrol_events"),
+            }
+            | params["filter_patrol_events"],
+            method="call",
+        ),
+        "pe_add_temporal_index": Node(
+            async_task=add_temporal_index.validate().set_executor("lithops"),
+            partial={
+                "df": DependsOn("filter_patrol_events"),
+            }
+            | params["pe_add_temporal_index"],
+            method="call",
+        ),
+        "pe_classify": Node(
+            async_task=apply_classification.validate().set_executor("lithops"),
+            partial={
+                "geodataframe": DependsOn("filter_patrol_events"),
+            }
+            | params["pe_classify"],
+            method="call",
+        ),
+        "pe_colormap": Node(
+            async_task=apply_color_map.validate().set_executor("lithops"),
+            partial={
+                "geodataframe": DependsOn("pe_classify"),
+            }
+            | params["pe_colormap"],
+            method="call",
+        ),
+        "pe_map_layer": Node(
+            async_task=create_map_layer.validate().set_executor("lithops"),
+            partial={
+                "geodataframe": DependsOn("pe_colormap"),
+            }
+            | params["pe_map_layer"],
+            method="call",
+        ),
+        "pe_ecomap": Node(
+            async_task=draw_ecomap.validate().set_executor("lithops"),
+            partial={
+                "geo_layers": DependsOn("pe_map_layer"),
+            }
+            | params["pe_ecomap"],
+            method="call",
+        ),
+        "pe_ecomap_html_url": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial={
+                "text": DependsOn("pe_ecomap"),
+                "root_path": os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
+            }
+            | params["pe_ecomap_html_url"],
+            method="call",
+        ),
+        "pe_map_widget": Node(
+            async_task=create_map_widget_single_view.validate().set_executor("lithops"),
+            partial={
+                "data": DependsOn("pe_ecomap_html_url"),
+            }
+            | params["pe_map_widget"],
+            method="call",
+        ),
+        "pe_bar_chart": Node(
+            async_task=draw_time_series_bar_chart.validate().set_executor("lithops"),
+            partial={
+                "dataframe": DependsOn("filter_patrol_events"),
+            }
+            | params["pe_bar_chart"],
+            method="call",
+        ),
+        "pe_bar_chart_html_url": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial={
+                "text": DependsOn("pe_bar_chart"),
+                "root_path": os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
+            }
+            | params["pe_bar_chart_html_url"],
+            method="call",
+        ),
+        "pe_bar_chart_widget": Node(
+            async_task=create_plot_widget_single_view.validate().set_executor(
+                "lithops"
+            ),
+            partial={
+                "data": DependsOn("pe_bar_chart_html_url"),
+            }
+            | params["pe_bar_chart_widget"],
+            method="call",
+        ),
+        "pe_meshgrid": Node(
+            async_task=create_meshgrid.validate().set_executor("lithops"),
+            partial={
+                "aoi": DependsOn("filter_patrol_events"),
+            }
+            | params["pe_meshgrid"],
+            method="call",
+        ),
+        "pe_feature_density": Node(
+            async_task=calculate_feature_density.validate().set_executor("lithops"),
+            partial={
+                "geodataframe": DependsOn("filter_patrol_events"),
+                "meshgrid": DependsOn("pe_meshgrid"),
+            }
+            | params["pe_feature_density"],
+            method="call",
+        ),
+        "fd_map_layer": Node(
+            async_task=create_map_layer.validate().set_executor("lithops"),
+            partial={
+                "geodataframe": DependsOn("pe_feature_density"),
+            }
+            | params["fd_map_layer"],
+            method="call",
+        ),
+        "fd_ecomap": Node(
+            async_task=draw_ecomap.validate().set_executor("lithops"),
+            partial={
+                "geo_layers": DependsOn("fd_map_layer"),
+            }
+            | params["fd_ecomap"],
+            method="call",
+        ),
+        "fd_ecomap_html_url": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial={
+                "text": DependsOn("fd_ecomap"),
+                "root_path": os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
+            }
+            | params["fd_ecomap_html_url"],
+            method="call",
+        ),
+        "fd_map_widget": Node(
+            async_task=create_map_widget_single_view.validate().set_executor("lithops"),
+            partial={
+                "data": DependsOn("fd_ecomap_html_url"),
+            }
+            | params["fd_map_widget"],
+            method="call",
+        ),
+        "split_patrol_event_groups": Node(
+            async_task=split_groups.validate().set_executor("lithops"),
+            partial={
+                "df": DependsOn("pe_add_temporal_index"),
+                "groupers": DependsOn("groupers"),
+            }
+            | params["split_patrol_event_groups"],
+            method="call",
+        ),
+        "grouped_pe_classify": Node(
+            async_task=apply_classification.validate().set_executor("lithops"),
+            partial=params["grouped_pe_classify"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("split_patrol_event_groups"),
+            },
+        ),
+        "grouped_pe_colormap": Node(
+            async_task=apply_color_map.validate().set_executor("lithops"),
+            partial=params["grouped_pe_colormap"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("grouped_pe_classify"),
+            },
+        ),
+        "grouped_pe_map_layer": Node(
+            async_task=create_map_layer.validate().set_executor("lithops"),
+            partial=params["grouped_pe_map_layer"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("grouped_pe_colormap"),
+            },
+        ),
+        "grouped_pe_ecomap": Node(
+            async_task=draw_ecomap.validate().set_executor("lithops"),
+            partial=params["grouped_pe_ecomap"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("grouped_pe_map_layer"),
+            },
+        ),
+        "grouped_pe_ecomap_html_url": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial=params["grouped_pe_ecomap_html_url"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["text"],
+                "argvalues": DependsOn("grouped_pe_ecomap"),
+            },
+        ),
+        "grouped_pe_map_widget": Node(
+            async_task=create_map_widget_single_view.validate().set_executor("lithops"),
+            partial=params["grouped_pe_map_widget"],
+            method="map",
+            kwargs={
+                "argnames": ["view", "data"],
+                "argvalues": DependsOn("grouped_pe_ecomap_html_url"),
+            },
+        ),
+        "grouped_pe_pie_chart": Node(
+            async_task=draw_pie_chart.validate().set_executor("lithops"),
+            partial=params["grouped_pe_pie_chart"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["dataframe"],
+                "argvalues": DependsOn("split_patrol_event_groups"),
+            },
+        ),
+        "grouped_pe_pie_chart_html_urls": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial={
+                "root_path": os.environ["ECOSCOPE_WORKFLOWS_RESULTS"],
+            }
+            | params["grouped_pe_pie_chart_html_urls"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["text"],
+                "argvalues": DependsOn("grouped_pe_pie_chart"),
+            },
+        ),
+        "grouped_pe_pie_chart_widgets": Node(
+            async_task=create_plot_widget_single_view.validate().set_executor(
+                "lithops"
+            ),
+            partial=params["grouped_pe_pie_chart_widgets"],
+            method="map",
+            kwargs={
+                "argnames": ["view", "data"],
+                "argvalues": DependsOn("grouped_pe_pie_chart_html_urls"),
+            },
+        ),
+        "grouped_pe_pie_widget_grouped": Node(
+            async_task=merge_widget_views.validate().set_executor("lithops"),
+            partial={
+                "widgets": DependsOn("grouped_pe_pie_chart_widgets"),
+            }
+            | params["grouped_pe_pie_widget_grouped"],
+            method="call",
+        ),
+        "grouped_pe_feature_density": Node(
+            async_task=calculate_feature_density.validate().set_executor("lithops"),
+            partial={
+                "meshgrid": DependsOn("pe_meshgrid"),
+            }
+            | params["grouped_pe_feature_density"],
+            method="map",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("split_patrol_event_groups"),
+            },
+        ),
+        "grouped_fd_map_layer": Node(
+            async_task=create_map_layer.validate().set_executor("lithops"),
+            partial=params["grouped_fd_map_layer"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geodataframe"],
+                "argvalues": DependsOn("pe_feature_density"),
+            },
+        ),
+        "grouped_fd_ecomap": Node(
+            async_task=draw_ecomap.validate().set_executor("lithops"),
+            partial=params["grouped_fd_ecomap"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["geo_layers"],
+                "argvalues": DependsOn("grouped_fd_map_layer"),
+            },
+        ),
+        "grouped_fd_ecomap_html_url": Node(
+            async_task=persist_text.validate().set_executor("lithops"),
+            partial=params["grouped_fd_ecomap_html_url"],
+            method="mapvalues",
+            kwargs={
+                "argnames": ["text"],
+                "argvalues": DependsOn("grouped_fd_ecomap"),
+            },
+        ),
+        "grouped_fd_map_widget": Node(
+            async_task=create_map_widget_single_view.validate().set_executor("lithops"),
+            partial=params["grouped_fd_map_widget"],
+            method="map",
+            kwargs={
+                "argnames": ["view", "data"],
+                "argvalues": DependsOn("grouped_fd_ecomap_html_url"),
+            },
+        ),
+        "patrol_dashboard": Node(
+            async_task=gather_dashboard.validate().set_executor("lithops"),
+            partial={
+                "widgets": DependsOnSequence(
+                    [
+                        DependsOn("pe_map_widget"),
+                        DependsOn("pe_bar_chart_widget"),
+                        DependsOn("fd_map_widget"),
+                        DependsOn("grouped_pe_map_widget"),
+                        DependsOn("grouped_pe_pie_widget_grouped"),
+                        DependsOn("grouped_fd_map_widget"),
+                    ],
+                ),
+                "groupers": DependsOn("groupers"),
+            }
+            | params["patrol_dashboard"],
+            method="call",
+        ),
+    }
+    graph = Graph(dependencies=dependencies, nodes=nodes)
+    results = graph.execute()
+    print(results)
