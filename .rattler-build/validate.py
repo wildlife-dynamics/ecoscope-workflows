27d0603ba193627bd2baeb3fc92c206e79191f6d
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-05 16:18:20 -0700

    congrats messages and note to-dos

--- .rattler-build/validate.py
+++ .rattler-build/validate.py
@@ -57,10 +57,24 @@ class RattlerBuildRecipe(BaseModel):
 if __name__ == "__main__":
     pyproject_dict = load_pyproject()
     pyproject = Pyproject(**pyproject_dict)
-    print(pyproject)
+    print("pyproject.toml: ", pyproject)
     recipe_dict = load_rattler_build_recipe()
     recipe = RattlerBuildRecipe(**recipe_dict)
-    print(recipe)
+    print("rattler-build recipe: ", recipe)
 
     for req in pyproject.project.dependencies:
         assert req.name in {r.name for r in recipe.requirements.run}
+
+    print(
+        "🎉 All pyproject.toml required dependencies are also present in rattler-build recipe! 🎉 "
+    )
+    # TODO:
+    # - Check python version in pyproject.toml against rattler-build recipe
+    # - Check host dependencies in pyproject.toml against rattler-build recipe
+    # - Check "Specifiers" compatibility (e.g. >=, ==, etc.). This is not entirely trivial because of the different
+    #   ways that dependencies can be specified in pyproject.toml and rattler-build recipes. (e.g. git urls, etc.).
+    # - Address the fact that while the definition of "latest" on the rattler-build side is pinned to our local channel
+    #   (for packages on the local channel, e.g. lithops, lonboard fork, etc.), the definition of "latest" on the
+    #   pyproject.toml side is pinned to the latest version available on PyPI. This is a potential source of inconsistency,
+    #   and may need to be addressed by a nightly check of PyPI for new versions of packages that we depend on (or similar).
+    # - Explicit optionals variants for rattler recipe, and compare these against pyproject.toml
