a4b79089d181d8c16d002ad2b0a76ee8ef3bf5c2
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-05 17:01:24 -0700

    revert compiler optional deps

--- ecoscope_workflows/compiler.py
+++ ecoscope_workflows/compiler.py
@@ -7,6 +7,7 @@ import sys
 from importlib import resources
 from typing import Annotated, Any, Callable, Literal, TypeAlias, TypeVar
 
+from jinja2 import Environment, FileSystemLoader
 from pydantic import (
     BaseModel,
     ConfigDict,
@@ -533,14 +534,6 @@ class DagCompiler(BaseModel):
 
     @ruff_formatted
     def generate_dag(self) -> str:
-        try:
-            from jinja2 import Environment, FileSystemLoader
-        except ImportError as e:
-            raise ImportError(
-                "The `jinja2` package is required to compile DAGs. "
-                "Please install `ecoscope-workflows[compile]` dependencies."
-            ) from e
-
         env = Environment(loader=FileSystemLoader(self.template_dir))
         template = env.get_template(self.template)
         return template.render(self.get_dag_config())
--- ecoscope_workflows/registry.py
+++ ecoscope_workflows/registry.py
@@ -11,6 +11,7 @@ from importlib.metadata import entry_points
 from inspect import getmembers, ismodule
 from typing import Annotated, Any, Generator, get_args
 
+import ruamel.yaml
 from pydantic import (
     BaseModel,
     Field,
@@ -187,14 +188,6 @@ class KnownTask(BaseModel):
         description: str,
         omit_args: list[str] | None = None,
     ) -> str:
-        try:
-            import ruamel.yaml
-        except ImportError as e:
-            raise ImportError(
-                "The `ruamel.yaml` package is required to generate YAML parameters. "
-                "Please install `ecoscope-workflows[compile]` dependencies."
-            ) from e
-
         yaml = ruamel.yaml.YAML(typ="rt")
         yaml_str = f"{description}\n{title}:\n"
         for line in self._iter_parameters_annotation(
