31fc9316d379546b9cd2397aa2e98b6c1732cd98
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-09 20:38:19 -0700

    test executors

--- src/ecoscope-workflows/core/ecoscope_workflows/core/decorators.py
+++ src/ecoscope-workflows/core/ecoscope_workflows/core/decorators.py
@@ -162,7 +162,7 @@ class _Task(Generic[P, R, K, V]):
                     executor=PythonExecutor(),
                 )
             case "lithops":
-                from ecoscope_workflows.executors.lithops import LithopsExecutor
+                from ecoscope_workflows.core.executors.lithops import LithopsExecutor
 
                 return AsyncTask(
                     self.func,
