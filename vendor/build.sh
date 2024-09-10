9e36e950606306b7605ae1c2e3e24bdbf9441793
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-09 23:32:55 -0700

    rebuild ecoscope vendor

--- vendor/build.sh
+++ vendor/build.sh
@@ -8,19 +8,10 @@ DEPENDENCIES=(
     "ecoscope-core"
 )
 
-PLATFORMS=(
-    "linux-aarch64"
-    "linux-64"
-    "osx-64"
-)
-
 for dep in "${DEPENDENCIES[@]}"; do
-    for platform in "${PLATFORMS[@]}"; do
-        echo "Building $dep for $platform"
-        rattler-build build \
-        --recipe $(pwd)/vendor/recipes/${dep}.yaml \
-        --target-platform $platform \
-        --output-dir $(pwd)/vendor/artifacts \
-        --skip-existing=all
-    done
+    echo "Building $dep"
+    rattler-build build \
+    --recipe $(pwd)/vendor/recipes/${dep}.yaml \
+    --output-dir $(pwd)/vendor/artifacts \
+    --skip-existing=all
 done
