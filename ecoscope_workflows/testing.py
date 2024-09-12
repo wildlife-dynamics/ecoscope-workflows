1cb34f405a3f6e80b03679edac6321d6d5dfb229
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-11 23:27:52 -0700

    move test_case to testing

--- ecoscope_workflows/testing.py
+++ ecoscope_workflows/testing.py
@@ -1,5 +1,10 @@
 import inspect
+import os
+import subprocess
+import sys
+from pathlib import Path
 
+import ruamel.yaml
 from pydantic import BaseModel
 
 from ecoscope_workflows.decorators import SyncTask
@@ -26,7 +31,66 @@ def create_task_magicmock(anchor: str, func_name: str) -> MockSyncTask:
     return MockSyncTask(func=mock_func, tags=task.tags, executor=task.executor)
 
 
+class Assertions(BaseModel):
+    result_stdout_contains: list[str]
+
+
 class TestCase(BaseModel):
     name: str
+    description: str
     params: dict
-    asserts: list
+    assertions: Assertions
+
+
+def test_case(script: Path, case_name: str, test_cases_yaml: Path, tmp_path: Path):
+    """Run a single test case for a workflow script."""
+
+    yaml = ruamel.yaml.YAML(typ="safe")
+    all_cases = yaml.load(test_cases_yaml.read_text())
+    assert case_name in all_cases, f"{case_name =} not found in {test_cases_yaml =}"
+    test_case = TestCase(**all_cases[case_name])
+    with tmp_path.joinpath("params.yaml").open("w") as f:
+        yaml.dump(test_case.params, f)
+
+    cmd = (
+        f"{sys.executable} -W ignore {script.absolute().as_posix()} "
+        f"--config-file {tmp_path.joinpath('params.yaml').as_posix()}"
+    )
+    env = os.environ.copy()
+    env["ECOSCOPE_WORKFLOWS_RESULTS"] = tmp_path.as_posix()
+    if "async" in script.as_posix() and not os.environ.get("LITHOPS_CONFIG_FILE"):
+        # a lithops test is requested but no lithops config is set in the environment
+        # users can set this in their environment to avoid this, to test particular
+        # lithops configurations. but if they don't, we'll set a default one here.
+        lithops_config_outpath = tmp_path / "lithops.yaml"
+        lithops_config = {
+            "lithops": {
+                "backend": "localhost",
+                "storage": "localhost",
+                "log_level": "INFO",
+                "data_limit": 16,
+            },
+            "localhost": {"runtime": sys.executable},
+        }
+        yaml = ruamel.yaml.YAML(typ="safe")
+        with open(lithops_config_outpath, mode="w") as f:
+            yaml.dump(lithops_config, f)
+
+        env["LITHOPS_CONFIG_FILE"] = lithops_config_outpath.as_posix()
+
+    proc = subprocess.Popen(
+        cmd.split(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        env=env,
+    )
+    returncode = proc.wait()
+    if returncode != 0:
+        assert proc.stderr is not None
+        raise ValueError(f"{cmd = } failed with:\n {proc.stderr.read()}")
+    assert returncode == 0
+    assert proc.stdout is not None
+    stdout = proc.stdout.read().strip()
+    for expected_substring in test_case.assertions.result_stdout_contains:
+        assert expected_substring in stdout
--- examples/mode-map/bld/test/test_workflow.py
+++ examples/mode-map/bld/test/test_workflow.py
@@ -1,10 +1,8 @@
-import os
-import sys
 from pathlib import Path
-import subprocess
 
 import pytest
-import ruamel.yaml
+
+from ecoscope_workflows.testing import test_case
 
 
 BLD = Path(__file__).parent.parent
@@ -19,53 +17,5 @@ TEST_CASES_YAML = BLD.parent / "test-cases.yaml"
 @pytest.mark.parametrize("script", WORKFLOWS, ids=[p.stem for p in WORKFLOWS])
 def test_end_to_end(script: Path, case: str, tmp_path: Path):
     print(f"Running end-to-end test for {script =} with {case =}:")
-    yaml = ruamel.yaml.YAML(typ="safe")
-    all_cases = yaml.load(TEST_CASES_YAML.read_text())
-    assert case in all_cases, f"{case =} not found in {TEST_CASES_YAML =}"
-    with tmp_path.joinpath("params.yaml").open("w") as f:
-        yaml.dump(all_cases[case]["params"], f)
-
-    cmd = (
-        f"{sys.executable} -W ignore {script.absolute().as_posix()} "
-        f"--config-file {tmp_path.joinpath('params.yaml').as_posix()}"
-    )
-    env = os.environ.copy()
-    env["ECOSCOPE_WORKFLOWS_RESULTS"] = tmp_path.as_posix()
-    if "async" in script.as_posix() and not os.environ.get("LITHOPS_CONFIG_FILE"):
-        # a lithops test is requested but no lithops config is set in the environment
-        # users can set this in their environment to avoid this, to test particular
-        # lithops configurations. but if they don't, we'll set a default one here.
-        lithops_config_outpath = tmp_path / "lithops.yaml"
-        lithops_config = {
-            "lithops": {
-                "backend": "localhost",
-                "storage": "localhost",
-                "log_level": "INFO",
-                "data_limit": 16,
-            },
-            "localhost": {"runtime": sys.executable},
-        }
-        yaml = ruamel.yaml.YAML(typ="safe")
-        with open(lithops_config_outpath, mode="w") as f:
-            yaml.dump(lithops_config, f)
-
-        env["LITHOPS_CONFIG_FILE"] = lithops_config_outpath.as_posix()
-
-    proc = subprocess.Popen(
-        cmd.split(),
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True,
-        env=env,
-    )
-    returncode = proc.wait()
-    if returncode != 0:
-        assert proc.stderr is not None
-        raise ValueError(f"{cmd = } failed with:\n {proc.stderr.read()}")
-    assert returncode == 0
-    assert proc.stdout is not None
-    stdout = proc.stdout.read().strip()
-    print(stdout)
-    # todo: get asserts from test case
-    # for assert_fn in end_to_end.assert_that_stdout:
-    #     assert assert_fn(stdout)
+    test_case(script, case, TEST_CASES_YAML, tmp_path)
+    print("Success!")
