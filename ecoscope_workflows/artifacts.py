73526dafb7d8777255dfc9d2b6a9a019a381fcd5
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-12 00:17:13 -0700

    some more artifact modeling

--- ecoscope_workflows/artifacts.py
+++ ecoscope_workflows/artifacts.py
@@ -1,8 +1,17 @@
+import json
+import sys
+from pathlib import Path
+
+if sys.version_info >= (3, 11):
+    pass
+else:
+    pass
+
 from pydantic import BaseModel
 
 
 class Dags(BaseModel):
-    """The target directory for the generated DAGs."""
+    """Target directory for the generated DAGs."""
 
     jupytext: str
     script_async_mock_io: str
@@ -10,16 +19,38 @@ class Dags(BaseModel):
     script_sequential_mock_io: str
     script_sequential: str
 
+    def dump(self, path: Path): ...
+
 
 class SymlinkVendor:
     pass
 
 
+class PixiToml(BaseModel):
+    """The pixi.toml file that specifies the workflow."""
+
+    name: str
+    version: (
+        str  # this can passthrough from the spec.yaml; like conda recipe build numbers
+    )
+    description: str
+    dependencies: list[str]
+
+    def dump(self, path: Path): ...
+
+
 class WorkflowArtifacts(BaseModel):
-    """The target directory for Pixi."""
+    model_config = dict(arbitrary_types_allowed=True)
 
     dags: Dags
     src: SymlinkVendor | None = None
     test: ...
     params_jsonschema: dict
     pixi_toml: dict  # if SymlinkVendor is None, we can simplify this and just install a release of workflows from prefix.dev
+
+    def dump(self, path: Path):
+        self.dags.dump(path / "dags")
+        self.pixi_toml.dump(path / "pixi.toml")
+
+        with path.joinpath("params.jsonschema").open("w") as f:
+            json.dump(self.params_jsonschema, f, indent=2)
