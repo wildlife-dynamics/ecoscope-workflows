f811a0919b985db4445045260c191904b12b883e
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-18 22:19:34 -0700

    ext.ecoscope doesn't have doctests

--- old/ecoscope_workflows/indexes.py
+++
@@ -1,6 +0,0 @@
-from typing import Literal, TypeAlias
-
-IndexName: TypeAlias = str
-IndexValue: TypeAlias = str
-Filter = tuple[IndexName, Literal["="], IndexValue]
-CompositeFilter = tuple[Filter, ...]
--- old/ecoscope_workflows/tasks/groupby/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._groupby import groupbykey, set_groupers, split_groups
-
-__all__ = ["groupbykey", "set_groupers", "split_groups"]
