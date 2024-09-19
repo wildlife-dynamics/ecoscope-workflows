7ded24023186f651a371df65d8c42a6b86d595d9
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-18 20:33:32 -0700

    add test annotations to core tests

---
+++ src/ecoscope-workflows/core/tests/test_annotations.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import pandera as pa
+import pandera.typing as pa_typing
+import pytest
+from pydantic import BaseModel, ValidationError
+
+from ecoscope_workflows.core.annotations import (
+    DataFrame,
+    JsonSerializableDataFrameModel,
+)
+
+
+def test_dataframe_type():
+    df = pd.DataFrame(data={"col1": [1, 2]})
+
+    class ValidSchema(JsonSerializableDataFrameModel):
+        col1: pa_typing.Series[int] = pa.Field(unique=True)
+
+    class ValidModel(BaseModel):
+        df: DataFrame[ValidSchema]
+
+    ValidModel(df=df)
+
+    class InvalidSchema(JsonSerializableDataFrameModel):
+        # Invalid because col1 elements are ints, not strings
+        col1: pa_typing.Series[str] = pa.Field(unique=True)
+
+    class InvalidModel(BaseModel):
+        df: DataFrame[InvalidSchema]
+
+    with pytest.raises(ValidationError):
+        InvalidModel(df=df)
