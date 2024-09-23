fc5b5d537ccd6df0db691f6568706ce3dbac764e
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-23 16:50:47 -0700

    rename templates

--- src/ecoscope-workflows-core/ecoscope_workflows_core/compiler.py
+++ src/ecoscope-workflows-core/ecoscope_workflows_core/compiler.py
@@ -661,7 +661,7 @@ class DagCompiler(BaseModel):
 
     @ruff_formatted
     def generate_dag(self, dag_type: DagTypes, mock_io: bool = False) -> str:
-        template = self._jinja_env.get_template(f"{dag_type}.jinja2")
+        template = self._jinja_env.get_template(f"run-{dag_type}.jinja2")
         testing = True if mock_io else False
         return template.render(
             self.get_dag_config(dag_type, mock_io=mock_io) | {"testing": testing}
