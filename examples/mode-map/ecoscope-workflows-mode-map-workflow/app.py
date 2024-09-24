e1a959a682d77c9b3d6de10d6333a457e06bd2fa
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-24 10:11:13 -0700

    app starts in docker without crashing

--- examples/mode-map/ecoscope-workflows-mode-map-workflow/app.py
+++ examples/mode-map/ecoscope-workflows-mode-map-workflow/app.py
@@ -1,17 +1,15 @@
-import json
 import os
 import subprocess
 import sys
+import tempfile
 import time
-from pathlib import Path
 from typing import Literal
 
-import httpx
-import yaml
-from fastapi import FastAPI, Response, status
+import ruamel.yaml
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.gzip import GZipMiddleware
-from pydantic import BaseModel, SecretStr
+from pydantic import SecretStr
 
 
 app = FastAPI(
@@ -29,84 +27,59 @@ app.add_middleware(
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
 
-class Params(BaseModel):
-    config_file_params: dict
-    data_connections_env_vars: dict[str, SecretStr]
+lithops_config = {
+    # TODO: all/most of this should be configurable
+    "lithops": {
+        "backend": "gcp_cloudrun",
+        "storage": "gcp_storage",
+        "log_level": "DEBUG",
+        "data_limit": 16,
+    },
+    "gcp": {
+        "region": "us-central1",
+        "credentials_path": os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
+    },
+    "gcp_cloudrun": {
+        "runtime": os.environ["LITHOPS_GCP_CLOUDRUN_RUNTIME"],
+        "runtime_cpu": 2,
+        "runtime_memory": 1000,
+    },
+}
 
 
-class Run(BaseModel):
-    execution_mode: Literal["async", "sequential"]
-    mock_io: bool
-    params: Params
-
-
-class WorkflowProcess(BaseModel):
-    returncode: int
-    stderr: str | None
-
-
-class UpdateResults(BaseModel):
-    status_code: int
-    detail: str | None
-
-
-class RunResponse(BaseModel):
-    workflow_process: WorkflowProcess
-    update_results: UpdateResults
-
-
-@app.post("/", response_model=RunResponse, status_code=200)
+@app.post("/", status_code=200)
 def run(
-    run: Run,
-    results_bucket: str,
+    entrypoint: str,
+    params: dict,
+    data_connections_env_vars: dict[str, SecretStr],
+    execution_mode: Literal["async", "sequential"],
+    mock_io: bool,
+    results_url: str,
+    lithops_config: dict,
     callback_url: str,  # TODO: authenticatation (hmac)
-
 ):
-    """Fetch and run a script with parameters and update the results on Ecoscope Server."""
-
-    tmp = Path(os.environ.get("TMPDIR", "/tmp"))
-
+    yaml = ruamel.yaml.YAML(typ="safe")
     # TODO: use parameters jsonschema to validate config_file_params
-    config_tmp = tmp / "config.yaml"
-    config_tmp.write_text(yaml.dump(script.params.config_file_params))
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".yaml") as config_file:
+        yaml.dump(params, config_file)
 
     cmd = (
-        f"{entrypoint} "
-        f"--config-file {tmp.joinpath('params.yaml').as_posix()} "
-        f"--execution-mode {runner.} "
+        f"pixi run -e default {entrypoint} "
+        f"--config-file {config_file.name} "
+        f"--execution-mode {execution_mode} "
         f'{("--mock-io" if mock_io else "--no-mock-io")}'
     )
+    with tempfile.NamedTemporaryFile(
+        delete=False, suffix=".yaml"
+    ) as lithops_config_file:
+        yaml.dump(lithops_config, lithops_config_file)
+
     env = (
         os.environ.copy()  # TODO: just copy PATH (not everything) from outer env?
-        | runner.popen.env.model_dump()
-        | {
-            k: v.get_secret_value()
-            for k, v in script.params.data_connections_env_vars.items()
-        }
+        | {k: v.get_secret_value() for k, v in data_connections_env_vars.items()}
+        | {"ECOSCOPE_WORKFLOWS_RESULTS": results_url}
+        | {"LITHOPS_CONFIG_FILE": lithops_config_file.name}
     )
-    if "async" in script.script_type:
-        lithops_config = {
-            # TODO: all/most of this should be configurable
-            "lithops": {
-                "backend": "gcp_cloudrun",
-                "storage": "gcp_storage",
-                "log_level": "DEBUG",
-                "data_limit": 16,
-            },
-            "gcp": {
-                "region": "us-central1",
-                "credentials_path": os.environ["GOOGLE_APPLICATION_CREDENTIALS"],
-            },
-            "gcp_cloudrun": {
-                "runtime": f"us.gcr.io/{os.environ['GCP_PROJECT']}/{env_name}-worker",
-                "runtime_cpu": 2,
-                "runtime_memory": 1000,
-            },
-        }
-        with open(tmp / "lithops_config.yaml", "w") as f:
-            yaml.dump(lithops_config, f)
-            env["LITHOPS_CONFIG_FILE"] = f.name
-
     stdout_logfile = os.environ.get(
         "STDOUT_LOGFILE", "/tmp/stdout.log"
     )  # TODO: figure out where we want to store logs
@@ -129,7 +102,6 @@ def run(
             stderr=stderr_writer,
             env=env,
             text=True,
-            shell=True,  # FIXME: can we avoid shell=True if using mamba?
         )
         while proc.poll() is None:
             # stream from buffers to stdout/stderr
@@ -149,34 +121,35 @@ def run(
         # from the log files for handling in the response below.
         proc_stdout = completed_proc_stdout_reader.read()
         proc_stderr = completed_proc_stderr_reader.read()
-    match returncode, proc_stdout, proc_stderr:
-        case (
-            0,  # exited success
-            str(),  # there is stdout
-            stderr,  # there might be stderr (i.e. warnings)
-        ):
-            patch_response = httpx.patch(
-                runner.ecoscope_server.update_results_endpoint,
-                headers=runner.ecoscope_server.headers,
-                content=json.dumps(proc_stdout, default=str).encode("utf-8"),
-            )
-            # TODO: handle case of non-200 response; implies needing to re-run workflow?
-            # Maybe re-running is not a big deal if we've cached the result, and is more
-            # stable than trying to recover from a failed update.
-            status_code = patch_response.status_code
-            detail = patch_response.text if status_code != 200 else None
-        case _, _, str():
-            stderr = proc_stderr
-            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            detail = None
-            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
-        case _, _, _:
-            stderr = "Unknown error"
-            status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            detail = None
-            response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    # match returncode, proc_stdout, proc_stderr:
+    #     case (
+    #         0,  # exited success
+    #         str(),  # there is stdout
+    #         stderr,  # there might be stderr (i.e. warnings)
+    #     ):
+    #         patch_response = httpx.patch(
+    #             callback_url,
+    #             headers=runner.ecoscope_server.headers,
+    #             content=json.dumps(proc_stdout, default=str).encode("utf-8"),
+    #         )
+    #         # TODO: handle case of non-200 response; implies needing to re-run workflow?
+    #         # Maybe re-running is not a big deal if we've cached the result, and is more
+    #         # stable than trying to recover from a failed update.
+    #         status_code = patch_response.status_code
+    #         detail = patch_response.text if status_code != 200 else None
+    #     case _, _, str():
+    #         stderr = proc_stderr
+    #         status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    #         detail = None
+    #         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+    #     case _, _, _:
+    #         stderr = "Unknown error"
+    #         status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    #         detail = None
+    #         response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
 
     return {
-        "workflow_process": {"returncode": returncode, "stderr": stderr},
-        "update_results": {"status_code": status_code, "detail": detail},
+        "returncode": returncode,
+        "proc_stdout": proc_stdout,
+        "proc_stderr": proc_stderr,
     }
