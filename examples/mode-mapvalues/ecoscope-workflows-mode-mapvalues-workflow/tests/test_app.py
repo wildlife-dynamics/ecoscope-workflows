e6372544856ac7329548766c108cb123d4807969
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-24 23:37:59 -0700

    test app WIP

--- examples/mode-mapvalues/ecoscope-workflows-mode-mapvalues-workflow/tests/test_app.py
+++ examples/mode-mapvalues/ecoscope-workflows-mode-mapvalues-workflow/tests/test_app.py
@@ -1,16 +1,24 @@
+from pathlib import Path
+
 from fastapi.testclient import TestClient
 
 from ecoscope_workflows_core.testing import TestCase
 
 
-def test_app(client: TestClient, execution_mode: str, mock_io: bool, case: TestCase):
+def test_app(
+    client: TestClient,
+    execution_mode: str,
+    mock_io: bool,
+    case: TestCase,
+    tmp_path: Path,
+):
     response = client.post(
         "/",
         json={
+            "params": case.params,
             "execution_mode": execution_mode,
             "mock_io": mock_io,
-            "params": case.params,
-            "results_url": ...,
+            "results_url": tmp_path.as_posix(),
         },
     )
     assert response.status_code == 200
