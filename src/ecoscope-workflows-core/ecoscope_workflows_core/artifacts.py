b4daa16baf727f21fbdcc81383a524631336c107
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-24 17:16:31 -0700

    add test app to artifacts

--- src/ecoscope-workflows-core/ecoscope_workflows_core/artifacts.py
+++ src/ecoscope-workflows-core/ecoscope_workflows_core/artifacts.py
@@ -125,6 +125,29 @@ class PixiToml(_AllowArbitraryAndValidateAssignment):
             tomli_w.dump(self.model_dump(by_alias=True), f)
 
 
+TEST_APP = """\
+import pytest
+from fastapi.testclient import TestClient
+
+from ecoscope_workflows_core.testing import TestCase
+
+
+@pytest.mark.parametrize("execution_mode", ["async", "sequential"])
+@pytest.mark.parametrize("mock_io", [True], ids=["mock-io"])
+def test_app(client: TestClient, execution_mode: str, mock_io: bool, case: TestCase):
+    response = client.post(
+        "/",
+        json={
+            "execution_mode": execution_mode,
+            "mock_io": mock_io,
+            "params": case.params,
+            "results_url": ...,
+        },
+    )
+    assert response.status_code == 200
+"""
+
+
 TEST_CLI = """\
 from pathlib import Path
 
