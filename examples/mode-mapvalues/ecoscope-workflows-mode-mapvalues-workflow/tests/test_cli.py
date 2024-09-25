16b8c2709d7ee942d792e246fe97b3c0cc4fe498
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-24 22:44:32 -0700

    recompile

--- examples/mode-mapvalues/ecoscope-workflows-mode-mapvalues-workflow/tests/test_cli.py
+++ examples/mode-mapvalues/ecoscope-workflows-mode-mapvalues-workflow/tests/test_cli.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from ecoscope_workflows_core.testing import TestCase, run_test_case
+from ecoscope_workflows_core.testing import TestCase, run_cli_test_case
 
 
 def test_cli(
@@ -10,4 +10,4 @@ def test_cli(
     case: TestCase,
     tmp_path: Path,
 ):
-    run_test_case(entrypoint, execution_mode, mock_io, case, tmp_path)
+    run_cli_test_case(entrypoint, execution_mode, mock_io, case, tmp_path)
