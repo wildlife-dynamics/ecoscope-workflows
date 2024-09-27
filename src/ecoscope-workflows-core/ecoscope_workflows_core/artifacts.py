89f92822e6cf11f149ab3a7da108c6af04a988db
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-27 16:25:25 -0700

    drop xfail

--- src/ecoscope-workflows-core/ecoscope_workflows_core/artifacts.py
+++ src/ecoscope-workflows-core/ecoscope_workflows_core/artifacts.py
@@ -162,12 +162,9 @@ def test_app(
 TEST_CLI = """\
 from pathlib import Path
 
-import pytest
-
 from ecoscope_workflows_core.testing import TestCase, run_cli_test_case
 
 
-@pytest.mark.xfail(reason="Params parsing behavior is broken; try using click.testing.CliRunner")
 def test_cli(
     entrypoint: str,
     execution_mode: str,
