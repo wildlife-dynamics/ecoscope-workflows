c64d3333afc460bb1cdf7626184b8a1e738ecdf2
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-27 16:13:46 -0700

    note flags

--- dev/recompile.sh
+++ dev/recompile.sh
@@ -3,7 +3,7 @@
 example=$1
 flags=$2
 
-echo "recompiling example: ${example}"
+echo "recompiling 'examples/${example}/spec.yaml' with flags '--clobber ${flags}'"
 
 command="pixi run --manifest-path src/ecoscope-workflows-ext-ecoscope/pyproject.toml --locked -e default \
 ecoscope-workflows compile --spec examples/${example}/spec.yaml --clobber ${flags}"
