9f0011d1b1b7924cd4b62850812b94fb46af8de4
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-28 00:03:23 -0700

    check container status

--- dev/docker-run.sh
+++ dev/docker-run.sh
@@ -3,4 +3,4 @@
 example=$1
 results_dir=.ecoscope-workflows-tmp/${example}-workflow-results
 
-docker run -d -v $(pwd)/${results_dir}:/workflow/results -e PORT=4000 -p 4000:4000 ecoscope-workflows-${example}-workflow
+docker run --name ${example} -d -v $(pwd)/${results_dir}:/workflow/results -e PORT=4000 -p 4000:4000 ecoscope-workflows-${example}-workflow
