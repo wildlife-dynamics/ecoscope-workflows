091d6d8cab84d97296f0ba3ad84d9dacd16b4f6a
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-27 23:36:00 -0700

    add docker run and invoke scripts

---
+++ dev/curl-invoke-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+example=$1
+execution_mode=sequential
+mock_io=true
+port=4000
+results_url=/workflow/results  # must be consistent with volume mount set in docker-run.sh
+params=$(yq -c '.test1.params' examples/${example}/test-cases.yaml)
+
+curl -X POST "http://localhost:${port}/?execution_mode=${execution_mode}&mock_io=${mock_io}&results_url=${results_url}" \
+-H "Content-Type: application/json" \
+-d '{"params": '"${params}"'}'
