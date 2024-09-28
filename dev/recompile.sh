a26f4d962d9c0d40621acd34a5edaff146fd54f8
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-09-27 22:18:23 -0700

    graphs work

--- dev/recompile.sh
+++ dev/recompile.sh
@@ -4,6 +4,9 @@ example=$1
 shift
 flags=$*
 
+# (re)initialize dot executable to ensure graphviz is available
+pixi run --manifest-path src/ecoscope-workflows-ext-ecoscope/pyproject.toml --locked -e default dot -c
+
 echo "recompiling 'examples/${example}/spec.yaml' with flags '--clobber ${flags}'"
 
 command="pixi run --manifest-path src/ecoscope-workflows-ext-ecoscope/pyproject.toml --locked -e default \
